a9a3ee518e0c753f6279b6015e13ce6729a99101
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-12-01 19:13:30 +0000

    Solve day 1 challenge

---
+++ javascript/day-1/index.js
@@ -0,0 +1,11 @@
+const { findSum } = require('./find-sum');
+const input = require('../../inputs/day-1.json');
+
+function main() {
+  const p1 = findSum(input, 2020, 2);
+  const p2 = findSum(input, 2020, 3);
+  console.log(`Part 1: ${p1.join(' * ')} = ${p1.reduce((acc, e) => acc * e)}`);
+  console.log(`Part 1: ${p2.join(' * ')} = ${p2.reduce((acc, e) => acc * e)}`);
+}
+
+main();
