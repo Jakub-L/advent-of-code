607cf8e43a9e47f0f3cdd30cf4636d06e363e73a
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-12-03 09:39:12 +0000

    Fix typos

--- javascript/day-1/index.js
+++ javascript/day-1/index.js
@@ -5,7 +5,7 @@ function main() {
   const p1 = findSum(input, 2020, 2);
   const p2 = findSum(input, 2020, 3);
   console.log(`Part 1: ${p1.join(' * ')} = ${p1.reduce((acc, e) => acc * e)}`);
-  console.log(`Part 1: ${p2.join(' * ')} = ${p2.reduce((acc, e) => acc * e)}`);
+  console.log(`Part 2: ${p2.join(' * ')} = ${p2.reduce((acc, e) => acc * e)}`);
 }
 
 main();
