cc0520e37ac2360b30103ecc58a24dabb93eea82
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-12-08 19:27:10 +0000

    Solve day 8 challenge

---
+++ javascript/day-8/index.js
@@ -0,0 +1,9 @@
+const { cpu, fixIncorrectInstruction } = require('./utils');
+const input = require('../../inputs/day-8.json');
+
+function main() {
+  console.log(`Part 1: ${cpu(input).accumulator}`);
+  console.log(`Part 2: ${fixIncorrectInstruction(input)}`);
+}
+
+main();
