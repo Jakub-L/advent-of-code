0409caa8f720600cc97aea58ed44631139150cb6
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-12-11 20:34:28 +0000

    Solve D11/P2

--- javascript/day-11/utils.js
+++ javascript/day-11/utils.js
@@ -1,12 +1,12 @@
 /**
  * Counts the number of neighbouring (incl. diagonal) occupied seats
  * @param {string[]} plan - Floorplan of the waiting area
- * @param {number} r - Index of row of seat of interest
- * @param {number} c - Index of column of seat of interest
- * @param {string="#"} char - Character defining what counts as "occupied seat"
+ * @param {number} seatX - Index of row of seat of interest
+ * @param {number} seatY - Index of column of seat of interest
+ * @param {number} maxDistance - Maximum distance to look for neighbours
  * @returns {number} Number of occupied seats surrounding seat of interest
  */
-const countTakenNeighbours = (plan, r, c, char = '#') =>
+const countNeighbours = (plan, seatX, seatY, maxDistance) =>
   [
     [-1, -1],
     [-1, 0],
@@ -15,24 +15,36 @@ const countTakenNeighbours = (plan, r, c, char = '#') =>
     [0, 1],
     [1, -1],
     [1, 0],
-    [1, 1],
-  ].reduce((acc, [dR, dC]) => acc + (plan[r + dR]?.[c + dC] === char), 0);
+    [1, 1]
+  ].reduce((acc, [dX, dY]) => {
+    for (let distance = 1; distance <= maxDistance; distance += 1) {
+      const [x, y] = [seatX + distance * dX, seatY + distance * dY];
+      const [maxX, maxY] = [plan.length, plan[x]?.length];
+      if (x < 0 || x >= maxX || y < 0 || y >= maxY || plan[x][y] === 'L')
+        return acc;
+      if (plan[x][y] === '#') return acc + 1;
+    }
+    return acc;
+  }, 0);
 
 /**
  * Finds the number of seats occupied after it has stabilised
  * @param {string[]} initialPlan - Floorplan of the waiting area in initial arrangement
+ * @param {number=1} maxDist - Maximum distance to look for neighbours
+ * @param {number=3} maxNeigh - Maximum number of neighbours before the seat is vacated
  * @returns {number} count of occupied seats
  */
-const findStablyOccupiedSeats = (initialPlan) => {
+const findStablyOccupiedSeats = (initialPlan, maxDist = 1, maxNeigh = 3) => {
   const rules = {
-    L: (plan, i, j) => (countTakenNeighbours(plan, i, j) === 0 ? '#' : 'L'),
-    '#': (plan, i, j) => (countTakenNeighbours(plan, i, j) >= 4 ? 'L' : '#'),
-    '.': () => '.',
+    L: (plan, i, j) => (countNeighbours(plan, i, j, maxDist) === 0 ? '#' : 'L'),
+    '#': (plan, i, j) =>
+      countNeighbours(plan, i, j, maxDist) > maxNeigh ? 'L' : '#',
+    '.': () => '.'
   };
   let area = {
     plan: initialPlan,
     changes: Infinity,
-    occupied: 0,
+    occupied: 0
   };
 
   while (area.changes !== 0) {
@@ -51,7 +63,7 @@ const findStablyOccupiedSeats = (initialPlan) => {
       {
         plan: [],
         changes: 0,
-        occupied: 0,
+        occupied: 0
       }
     );
   }
