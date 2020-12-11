8e187aec27a015c999335944126e22c226c4e317
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-12-11 15:57:54 +0000

    Add an O(n) solution to D10/P2

--- javascript/day-10/index.js
+++ javascript/day-10/index.js
@@ -1,10 +1,15 @@
-const { findJoltDiffs, findAdapterCombinations } = require('./utils');
+const {
+  findJoltDiffs,
+  treeCombinations,
+  arrayCombinations,
+} = require('./utils');
 const input = require('../../inputs/day-10.json');
 
 function main() {
   const p1 = findJoltDiffs(input);
   console.log(`Part 1: ${p1[1] * p1[3]}`);
-  console.log(`Part 2: ${findAdapterCombinations(input)}`);
+  console.log(`Part 2 (tree):  ${treeCombinations(input)}`);
+  console.log(`Part 2 (array): ${arrayCombinations(input)}`);
 }
 
 main();
--- javascript/day-10/utils.js
+++ javascript/day-10/utils.js
@@ -28,11 +28,11 @@ const findJoltDiffs = (jolts) => {
  * @param {number[]=[1,2,3]} joltRange - Possible joltage differences
  * @returns {number} Number of ways to reach the device's joltage
  */
-const findAdapterCombinations = (jolts, start = 0, joltRange = [1, 2, 3]) => {
+const treeCombinations = (jolts, start = 0, joltRange = [1, 2, 3]) => {
   // mapped: true if all descendants have been explored
   // leaves: number of descendants with no children
   const tree = jolts.reduce(
-    (tree, jolt) => ({ ...tree, [jolt]: { mapped: false, leaves: 0 } }),
+    (acc, jolt) => ({ ...acc, [jolt]: { mapped: false, leaves: 0 } }),
     { [start]: { mapped: false, leaves: 0 } }
   );
   /**
@@ -46,7 +46,7 @@ const findAdapterCombinations = (jolts, start = 0, joltRange = [1, 2, 3]) => {
       []
     );
     for (let i = 0; i < children.length; i += 1) {
-      let child = children[i];
+      const child = children[i];
       if (tree[child].mapped) tree[val].leaves += tree[child].leaves;
       else tree[val].leaves += recurseTree(child);
     }
@@ -58,4 +58,33 @@ const findAdapterCombinations = (jolts, start = 0, joltRange = [1, 2, 3]) => {
   return tree[0].leaves;
 };
 
-module.exports = { findJoltDiffs, findAdapterCombinations };
+/**
+ * Finds the number of combinations of adapters with an array based approach
+ * Heavily based on work by J. Stothard (https://github.com/jstothard)
+ * @param {number[]} jolts - Array of nominal joltages for each adapter
+ * @param {number[]=[1,2,3]} joltRange - Possible joltage differences
+ * @returns {number} Number of ways to reach the device's joltage
+ */
+const arrayCombinations = (jolts, joltRange = [1, 2, 3]) => {
+  const joltDict = jolts.reduce(
+    (acc, jolt) => ({
+      ...acc,
+      [jolt]: true,
+      deviceJoltage: Math.max(acc.deviceJoltage, jolt + 3),
+    }),
+    { deviceJoltage: 3 }
+  );
+  // routes[n] is the number of routes to reach joltage of n
+  const routes = { 0: 1 };
+  for (let i = 1; i <= joltDict.deviceJoltage; i += 1) {
+    if (i in joltDict || i === joltDict.deviceJoltage) {
+      routes[i] = joltRange.reduce(
+        (sum, diff) => sum + (routes[i - diff] || 0),
+        0
+      );
+    }
+  }
+  return routes[joltDict.deviceJoltage];
+};
+
+module.exports = { findJoltDiffs, treeCombinations, arrayCombinations };
