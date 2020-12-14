77456d875932ff2d04ed0b6c77fe1d12982ace64
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-12-14 19:17:33 +0000

    Solve D14/P2

--- javascript/day-14/index.js
+++ javascript/day-14/index.js
@@ -2,14 +2,8 @@ const { sumMemory } = require('./utils');
 const input = require('../../inputs/day-14.json');
 
 function main() {
-  const testInput = [
-    'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
-    'mem[8] = 11',
-    'mem[7] = 101',
-    'mem[8] = 0',
-  ];
   console.log(`Part 1: ${sumMemory(input)}`);
-  //   console.log(`Part 2: ${findSubsequentTime(input)}`);
+  console.log(`Part 2: ${sumMemory(input, true)}`);
 }
 
 main();
--- javascript/day-14/utils.js
+++ javascript/day-14/utils.js
@@ -12,13 +12,48 @@ const maskValue = (mask, value) => {
   return parseInt(masked, 2);
 };
 
-const sumMemory = (instructions) => {
+/**
+ * Decodes memory addresses using provided mask
+ * @param {string} mask - Masking string, "1" and "0" switch bits to those numbers, "X" leaves unchanged
+ * @param {number} source - Source memory address to go through the masking/floating process
+ * @returns {number[]} Decimal representation of all addresses after floating bits are considered
+ */
+const decodeMemoryAddresses = (mask, source) => {
+  let addresses = [''];
+  const bin = source.toString(2).padStart(36, '0');
+  // Mask source address
+  for (const i in mask) {
+    addresses = addresses.reduce((acc, e) => {
+      if (mask[i] === '1') return [...acc, `${e}1`];
+      if (mask[i] === '0') return [...acc, `${e}${bin[i]}`];
+      if (mask[i] === 'X') return [...acc, `${e}1`, `${e}0`];
+    }, []);
+  }
+  return addresses.map((addr) => parseInt(addr, 2));
+};
+
+/**
+ * Finds the sum of the memory at the end of all instructions
+ * @param {string[]} instructions - List of memory and masking instructions
+ * @param {boolean=false} decoder - Whether the program should work as a memory address decoder
+ * @returns {number} Sum of all existing memory addresses
+ */
+const sumMemory = (instructions, decoder = false) => {
   const rules = {
     mask: (acc, mask) => ({ ...acc, mask }),
     mem: (acc, addr, val) => {
-      acc.mem[addr] = maskValue(acc.mask, Number(val));
+      if (decoder) {
+        decodeMemoryAddresses(acc.mask, Number(addr)).forEach((address) => {
+          acc.sum += Number(val) - (acc.mem[address] || 0);
+          acc.mem[address] = Number(val);
+        });
+      } else {
+        const newVal = maskValue(acc.mask, Number(val));
+        acc.sum += newVal - (acc.mem[addr] || 0);
+        acc.mem[addr] = newVal;
+      }
       return acc;
-    },
+    }
   };
   const finalState = instructions.reduce(
     (acc, instr) => {
@@ -28,9 +63,10 @@ const sumMemory = (instructions) => {
     {
       mem: [],
       mask: '',
+      sum: 0
     }
   );
-  return finalState.mem.reduce((sum, val) => sum + val, 0);
+  return finalState.sum;
 };
 
 module.exports = { sumMemory };
