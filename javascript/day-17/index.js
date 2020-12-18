ac404954679713a52554ac81b416a78b3d3e4c4d
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-12-18 16:52:57 +0000

    Remove test input

--- javascript/day-17/index.js
+++ javascript/day-17/index.js
@@ -2,8 +2,6 @@ const { countActiveFields } = require('./utils');
 const input = require('../../inputs/day-17.json');
 
 function main() {
-  const testInput = ['.#.', '..#', '###'];
-
   console.log(`Part 1: ${countActiveFields(input, 6, 3)}`);
   console.log(`Part 2: ${countActiveFields(input, 6, 4)}`);
 }
