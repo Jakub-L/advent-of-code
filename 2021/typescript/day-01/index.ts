d99822bb0542fd7287e7f27c1c2c0ff794279642
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2021-12-01 12:10:32 +0000

    Genericise the function from Part 2 to work for Part 1 as well

--- 2021/typescript/day-01/index.ts
+++ 2021/typescript/day-01/index.ts
@@ -9,38 +9,24 @@ const depths: Array<number> = readInput('./../../inputs/day-01.txt').map(
   Number
 );
 
-// Part 1
+// PARTS 1 & 2
 /**
- * Counts the number of increasing measurements in an array of depths
- * @param {Array.<number>} depths - Array of depth measurements
- * @returns {number} Number of increasing measurements
- */
-const countIncreases = (depths: Array<number>): number =>
-  depths.reduce(
-    (acc, depth, i, arr) =>
-      i !== arr.length && depth < arr[i + 1] ? acc + 1 : acc,
-    0
-  );
-
-// Part 2
-/**
- * Counts the number of increasing measurements by considering a N-long window.
- * Sums N consecutive numbers in each window and compares the sum, rather
- * than individual values.
+ * Counts the number of increasing measurements, by considering N-length window of
+ * measurements. Sums N consecutive numbers in each window and compares the sums. If
+ * window = 1, this is equivalent to comparing each value individually.
  * @param {Array.<number>} depths - Array of depth measurements
  * @param {number} window - The length of the window to consider
  * @returns {number} Number of increasing 3-long measurement windows
  */
-const countSlidingWindowIncreases = (
-  depths: Array<number>,
-  window = 3
-): number =>
-  countIncreases(
-    depths.map((_, i) =>
-      depths.slice(i, i + window).reduce((acc, n) => acc + n, 0)
-    )
-  );
+const countIncreases = (depths: Array<number>, window: number): number =>
+  depths
+    .map((_, i) => depths.slice(i, i + window).reduce((acc, n) => acc + n, 0))
+    .reduce(
+      (acc, depth, i, arr) =>
+        i !== arr.length && depth < arr[i + 1] ? acc + 1 : acc,
+      0
+    );
 
 // Outputs
-console.log(`Part 1: ${countIncreases(depths)}`);
-console.log(`Part 2: ${countSlidingWindowIncreases(depths)}`);
+console.log(`Part 1: ${countIncreases(depths, 1)}`);
+console.log(`Part 2: ${countIncreases(depths, 3)}`);
