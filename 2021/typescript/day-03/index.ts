57faddcd72906ce544077a690e655dc06bc6af7a
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2021-12-03 11:28:49 +0000

    Simplify finding the bit frequency

--- 2021/typescript/day-03/index.ts
+++ 2021/typescript/day-03/index.ts
@@ -5,7 +5,9 @@
 import { readInput } from '../utils';
 
 // INPUTS
-const diagnostics = readInput('./../../inputs/day-03.txt');
+const diagnostics = readInput('./../../inputs/day-03.txt').map((num) =>
+  num.split('').map((d) => parseInt(d, 2))
+);
 const test = [
   '00100',
   '11110',
@@ -19,35 +21,38 @@ const test = [
   '11001',
   '00010',
   '01010'
-];
+].map((num) => num.split('').map((d) => parseInt(d, 2)));
 
 // UTILS
 
-
 // PART 1
-const countBits = (binaryStrings: string[]): any => {
-  const counts = new Array(binaryStrings[0].length)
-    .fill(null)
-    .map((e) => [0, 0]);
-  for (let str of binaryStrings) {
-    for (let i = 0; i < str.length; i += 1) {
-      const char = str[i];
-      const digit = parseInt(char, 10);
-      counts[i][digit] += 1;
-    }
-  }
-  return counts;
-};
-const findPowerConsumption = (bitCounts: any): number => {
-  let epsilon = '';
-  let gamma = '';
-  for (let pos of bitCounts) {
-    gamma = `${gamma}${pos[0] > pos[1] ? 0 : 1}`;
-    epsilon = `${epsilon}${pos[0] > pos[1] ? 1 : 0}`;
-  }
-  console.log(`epsilon ${epsilon}, gamma ${gamma}`);
-  return parseInt(epsilon, 2) * parseInt(gamma, 2);
-};
+/**
+ * Finds the gamma number, representing the most common positional binary digits.
+ * Sums the digits in each position, then for each position checks if the digit sum is
+ * higher than the length of the diagnostics / 2 - if so, the most common digit for
+ * that position is 1.
+ * @param {Array.<Array.<number>>} diag - Array of diagnostic binary diag, split into digits
+ * @returns {string} The binary representation of the most common binary digits
+ */
+const findGamma = (diag: number[][]): string =>
+  diag
+    .reduce((acc, number) => {
+      number.forEach((digit, i) => (acc[i] = digit + (acc[i] || 0)));
+      return acc;
+    }, [])
+    .reduce((gamma, count) => `${gamma}${+(count >= diag.length / 2)}`, '');
+
+/**
+ * Calculates the power consumption of the submarine
+ * @param {string} gamma - Binary number representing most common digits in diagnostics
+ * @returns {number} Power consumption of the submarine
+ */
+const findPowerConsumption = (gamma: string): number =>
+  parseInt(gamma, 2) *
+  (parseInt(gamma, 2) ^ parseInt('1'.repeat(gamma.length), 2));
+
+// PART 2
 
 // Outputs
-console.log(findPowerConsumption(countBits(numbers)));
+const gamma = findGamma(diagnostics);
+console.log(findPowerConsumption(gamma));
