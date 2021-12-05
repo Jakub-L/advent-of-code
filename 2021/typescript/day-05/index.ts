30c4455b7010e65e789daa57cb34f9f489083b91
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2021-12-05 20:01:51 +0000

    Improve documentation

--- 2021/typescript/day-05/index.ts
+++ 2021/typescript/day-05/index.ts
@@ -5,43 +5,36 @@
 import { readInput } from '../utils';
 
 // INPUTS
-const test = [
-  '0,9 -> 5,9',
-  '8,0 -> 0,8',
-  '9,4 -> 3,4',
-  '2,2 -> 2,1',
-  '7,0 -> 7,4',
-  '6,4 -> 2,0',
-  '0,9 -> 2,9',
-  '3,4 -> 1,4',
-  '0,0 -> 8,8',
-  '5,5 -> 8,2'
-].map((line) =>
-  line.split(' -> ').map((coords) => coords.split(',').map(Number))
-);
 const lines = readInput('./../../inputs/day-05.txt').map((line) =>
   line.split(' -> ').map((coords) => coords.split(',').map(Number))
 );
 
 // PART 1 & 2
+/**
+ * Find the number of times a set of lines overlaps
+ * @param {Array.<Array.<Array.<number>>>} lines - Array of lines to draw, each line a pair of number arrays
+ * which are the coordinates of the start and end points
+ * @param {boolean} countDiagonals - Should the script consider diagonal lines
+ * @returns {number} The number of points at which at least 2 lines overlap
+ */
 const countOverlaps = (
   lines: Array<Array<Array<number>>>,
   countDiagonals: boolean
 ): number => {
   const points: { [index: string]: number } = {};
   for (let line of lines) {
-    let [[x0, y0], [x1, y1]] = line;
-    const [dx, dy] = [Math.sign(x1 - x0), Math.sign(y1 - y0)];
+    let [[x, y], [x1, y1]] = line;
+    const [dx, dy] = [Math.sign(x1 - x), Math.sign(y1 - y)];
     if (!countDiagonals && dx !== 0 && dy !== 0) continue;
-    while (x0 !== x1 || y0 !== y1) {
-      points[`${x0},${y0}`] = 1 + (points[`${x0},${y0}`] || 0);
-      if (x0 !== x1) x0 = x0 + dx;
-      if (y0 !== y1) y0 = y0 + dy;
+    while (x !== x1 || y !== y1) {
+      points[`${x},${y}`] = 1 + (points[`${x},${y}`] || 0);
+      if (x !== x1) x += dx;
+      if (y !== y1) y += dy;
     }
-    points[`${x0},${y0}`] = 1 + (points[`${x0},${y0}`] || 0);
+    points[`${x},${y}`] = 1 + (points[`${x},${y}`] || 0);
   }
   return Object.values(points).reduce(
-    (acc, overlaps) => acc + +(overlaps > 1),
+    (acc, overlaps) => acc + (overlaps > 1 ? 1 : 0),
     0
   );
 };
