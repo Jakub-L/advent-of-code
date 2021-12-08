c539e47bffd92e91a737d53f7c0417e110b25459
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-08 09:54:34 +0000

    Solve day 7 part 2 using a more clever method, add documentation

--- 2021/typescript/day-07/index.ts
+++ 2021/typescript/day-07/index.ts
@@ -1,39 +1,59 @@
 /**
- * Solution to Day 6 challenge of Advent of Code 2021
- * https://adventofcode.com/2021/day/6
+ * Solution to Day 7 challenge of Advent of Code 2021
+ * https://adventofcode.com/2021/day/7
  */
 import { readInput } from '../utils';
 
 // INPUTS
 const crabs = readInput('./../../inputs/day-07.txt', ',').map(Number);
-const test = '16,1,2,0,4,2,7,1,2,14'.split(',').map(Number);
 
-// PART 1 & 2
-const findMedian = (crabs: Array<number>) => {
-  const medianIndex = Math.floor(crabs.length / 2) - 1;
-  return crabs.sort((a, b) => a - b)[medianIndex];
+// UTILS
+/**
+ * Finds the median of an array. For an even-length array, returns the nearest number below the mean
+ * @param {Array.<number>} arr - An array of numbers
+ * @returns {number} The median of the array
+ */
+const findMedian = (arr: Array<number>): number => {
+  const medianIndex = Math.floor(arr.length / 2) - 1;
+  return arr.sort((a, b) => a - b)[medianIndex];
 };
 
-const countFuel = (crabs: Array<number>) => {
+/**
+ * Finds the  mean of an array
+ * @param {Array.<number>} arr - An array of numbers
+ * @returns {number} The n mean of the array
+ */
+const findMean = (arr: Array<number>): number =>
+  arr.reduce((sum, n) => sum + n, 0) / arr.length;
+
+// PART 1
+/**
+ * Finds the minimum amount of fuel required to align crabs. Fuel cost is proportional to number of steps
+ * @param {Array.<number>} crabs - An array of numbers representing the initial crab positions
+ * @returns {number} The total minimal fuel expenditure
+ */
+const countMinimumFuel = (crabs: Array<number>): number => {
   const median = findMedian(crabs);
   return crabs.reduce((sum, crab) => sum + Math.abs(crab - median), 0);
 };
 
-const brutePart2 = (crabs: Array<number>) => {
-  let sum = 1000000000;
-  let newSum;
-  for (let t = 0; t < 2000; t++) {
-    newSum = crabs.reduce(
-      (acc, c) => acc + (Math.abs(c - t) * (Math.abs(c - t) + 1)) / 2,
-      0
-    );
-    if (newSum >= sum) return sum;
-    else sum = newSum;
-  }
+// PART 2
+/**
+ * Finds the minimum amount of fuel required to align crabs. Fuel cost is equal to N * (N + 1) / 2, where
+ * N is the number of units the crab must move into alignment
+ * @param {Array.<number>} crabs - An array of numbers representing the initial crab positions
+ * @returns {number} The total minimal fuel expenditure
+ */
+const countMinimumTriangularFuel = (crabs: Array<number>): number => {
+  // The target must be an integer position
+  const mean = Math.floor(findMean(crabs));
+  return crabs.reduce(
+    (sum, crab) =>
+      sum + (Math.abs(crab - mean) * (Math.abs(crab - mean) + 1)) / 2,
+    0
+  );
 };
 
 // OUTPUTS
-// console.log(countFuel(crabs));
-console.log(brutePart2(crabs));
-// console.log(`Part 1: ${countFish(fish, 80)}`);
-// console.log(`Part 2: ${countFish(fish, 256)}`);
+console.log(`Part 1: ${countMinimumFuel(crabs)}`);
+console.log(`Part 2: ${countMinimumTriangularFuel(crabs)}`);
