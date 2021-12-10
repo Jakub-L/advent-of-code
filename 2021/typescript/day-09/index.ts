c0fa0843dc047f7fcd7eb484bc9034e62bd0374b
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-10 12:43:11 +0000

    Add extra documentation to functions

--- 2021/typescript/day-09/index.ts
+++ 2021/typescript/day-09/index.ts
@@ -19,32 +19,42 @@ const test = [
 ].map((row) => row.split('').map(Number));
 
 // UTILS
+
+// PART 1
+/**
+ * Finds the depths of local minimum points. Does not look at diagonal neighbours.
+ * @param {number[][]} depths - 2D map of sea bed depths as digits from 0 to 9
+ * @returns {number[]} Array of the depths of the local minima
+ */
 const findMinima = (depths: number[][]): number[] =>
   depths.reduce(
-    (min, row, i) => [
-      ...min,
-      ...row.filter((num, j) =>
-        [
-          [i - 1, j],
-          [i, j - 1],
-          [i, j + 1],
-          [i + 1, j],
-        ].every(
-          ([row, col]) =>
-            depths[row]?.[col] === undefined || num < depths[row][col]
+    (min, row, i) =>
+      min.concat(
+        row.filter((num, j) =>
+          [
+            [i - 1, j],
+            [i, j - 1],
+            [i, j + 1],
+            [i + 1, j],
+          ].every(
+            ([row, col]) =>
+              depths[row]?.[col] === undefined || num < depths[row][col]
+          )
         )
       ),
-    ],
     []
   );
 
-// PART 1
-const sumRisk = (depths: number[][]): number =>
-  findMinima(depths).reduce((acc, minimum) => acc + minimum + 1, 0);
+/**
+ * Finds the sum or risks for minimum points, where risk = depth + 1
+ * @param {number[]} minima - Array of the depths of local minima
+ * @returns {number} Sum of risks across all minima
+ */
+const sumRisk = (minima: number[]): number =>
+  minima.reduce((acc, minimum) => acc + minimum + 1, 0);
 
 // PART 2
 
 // OUTPUTS
-console.log(sumRisk(depths));
-// console.log(`Part 1: ${}`);
+console.log(`Part 1: ${sumRisk(findMinima(depths))}`);
 // console.log(`Part 2: ${}`);
