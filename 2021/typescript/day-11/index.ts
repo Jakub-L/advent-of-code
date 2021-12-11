8376e8fbdb5f2b66481deed96631006a2a4eedf1
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2021-12-11 18:16:42 +0000

    Tidy up the solution, add documentation

--- 2021/typescript/day-11/index.ts
+++ 2021/typescript/day-11/index.ts
@@ -10,6 +10,7 @@ const octopuses = readInput('./../../inputs/day-11.txt').map((row) =>
 );
 
 // UTILS
+const FLASH_LIMIT = 9;
 const neighbours = [
   [-1, -1],
   [-1, 0],
@@ -21,44 +22,66 @@ const neighbours = [
   [1, 1]
 ];
 
+/** Class representing a cavern full of octopuses */
 class Cavern {
   grid: number[][];
-  gridSize: number;
+  size: number;
   flashes: number;
-  syncFlash: boolean;
 
+  /**
+   * Creates a new octopus-filled cavern
+   * @param {number[][]} octopuses - 2D grid of octopus energies, as integers
+   */
   constructor(octopuses: number[][]) {
     this.grid = octopuses;
-    this.gridSize = octopuses.reduce((acc, row) => acc + row.length, 0);
+    this.size = octopuses.reduce((acc, row) => acc + row.length, 0);
     this.flashes = 0;
-    this.syncFlash = false;
   }
 
+  /**
+   * Flashes an octopus and energises its neighbours
+   * @param {number} i - Row of the flashing octopus
+   * @param {number} j - Column of the flashing octopus
+   */
   flash(i: number, j: number) {
+    this.flashes += 1;
+    this.grid[i][j] = 0;
     neighbours.forEach(([di, dj]) => {
       if (this.grid[i + di]?.[j + dj]) this.grid[i + di][j + dj] += 1;
     });
-    this.flashes += 1;
   }
 
+  /**
+   * Increments the time by one step. Increases energy of all octopuses by 1
+   * and flashes any octopuses with energy > FLASH_LIMIT
+   */
   step() {
     this.grid = this.grid.map((row) => row.map((n) => n + 1));
-    let currCount = this.flashes;
     let flashed;
-
     do {
       flashed = false;
       this.grid.forEach((row, i) =>
         row.forEach((n, j) => {
-          if (n > 9) {
-            this.grid[i][j] = 0;
+          if (n > FLASH_LIMIT) {
             this.flash(i, j);
             flashed = true;
           }
         })
       );
     } while (flashed);
-    if (this.flashes - currCount === this.gridSize) this.syncFlash = true;
+  }
+
+  /**
+   * Finds the first point of synchronous flashing
+   * @returns {number} First step after which all octopuses flash synchronisedly
+   */
+  findSyncStep(): number {
+    let prevFlashes;
+    for (let i = 1; true; i += 1) {
+      prevFlashes = this.flashes;
+      this.step();
+      if (prevFlashes + this.size === this.flashes) return i;
+    }
   }
 }
 
@@ -67,16 +90,8 @@ const firstCavern = new Cavern(octopuses);
 for (let i = 0; i < 100; i += 1) firstCavern.step();
 
 // PART 2
-const findSyncStep = (cavern: Cavern): number => {
-  for (let i = 1; true; i += 1) {
-    cavern.step();
-    if (cavern.syncFlash) return i;
-  }
-};
-
 const secondCavern = new Cavern(octopuses);
-let syncStep = findSyncStep(secondCavern);
 
 // OUTPUTS
 console.log(`Part 1: ${firstCavern.flashes}`);
-console.log(`Part 2: ${syncStep}`);
+console.log(`Part 2: ${secondCavern.findSyncStep()}`);
