dd2be08f2b66e9885eeba6a8bcb3b3e7fad1f271
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-14 16:20:03 +0000

    Add input and file template for day 14

---
+++ 2021/typescript/day-14/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Solution to Day 14 challenge of Advent of Code 2021
+ * https://adventofcode.com/2021/day/14
+ */
+import { readInput } from '../utils';
+
+// INPUTS
+const input = readInput('./../../inputs/day-14.txt');
+
+// UTILS
+
+// PART 1
+
+// PART 2
+
+// OUTPUTS
+// console.log(`Part 1: ${}`);
+// console.log(`Part 2: ${}`);
