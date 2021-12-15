e4749ecde16b8b99beef82a06f3d11e9d65747df
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-15 12:21:56 +0000

    Add template for day 15 file

---
+++ 2021/typescript/day-15/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Solution to Day 15 challenge of Advent of Code 2021
+ * https://adventofcode.com/2021/day/15
+ */
+import { readInput } from '../utils';
+
+// INPUTS
+const input = readInput('./../../inputs/day-15.txt');
+
+// UTILS
+
+// PART 1
+
+// PART 2
+
+// OUTPUTS
+// console.log(`Part 1: ${}`);
+// console.log(`Part 2: ${}`);
