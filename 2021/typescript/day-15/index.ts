b6633b26fb4cefa725af7cdced24337e891e3d11
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2021-12-15 21:02:56 +0000

    Solve part 2

--- 2021/typescript/day-15/index.ts
+++ 2021/typescript/day-15/index.ts
@@ -2,53 +2,28 @@
  * Solution to Day 15 challenge of Advent of Code 2021
  * https://adventofcode.com/2021/day/15
  */
-import { readInput } from '../utils';
+import { readInput, gridShortestPath } from '../utils';
 
 // INPUTS
-const test = `1163751742
-1381373672
-2136511328
-3694931569
-7463417111
-1319128137
-1359912421
-3125421639
-1293138521
-2311944581`
-  .split('\n')
-  .map((row) => row.split('').map(Number));
-
 const input = readInput('./../../inputs/day-15.txt').map((row) =>
   row.split('').map(Number)
 );
 
-// UTILS
-const findDistance = (risks: number[][]) => {
-  const distances = Array(risks.length)
-    .fill(null)
-    .map((row, i) => Array(risks[i].length).fill(null));
-  for (let i = risks.length - 1; i >= 0; i--) {
-    for (let j = risks[i].length - 1; j >= 0; j--) {
-      const [down, right] = [distances[i + 1]?.[j], distances[i]?.[j + 1]];
-      let dist;
-      if (down && right) dist = Math.min(down, right);
-      else if (down) dist = down;
-      else if (right) dist = right;
-      else dist = 0;
-      distances[i][j] = dist + risks[i][j];
-    }
-  }
-  return distances;
-};
-
-// PART 1
-
 // PART 2
+const mult = 5;
+const [row, col] = [input.length, input[0].length];
+const bigGrid = Array(row * mult)
+  .fill(null)
+  .map((_, y) =>
+    Array(col * mult)
+      .fill(null)
+      .map((_, x) => {
+        const val =
+          input[y % row][x % col] + Math.floor(x / col) + Math.floor(y / row);
+        return val > 9 ? val % 9 : val;
+      })
+  );
 
 // OUTPUTS
-console.log(findDistance(input)[0][0] - input[0][0]);
-// console.log(`Part 1: ${}`);
-// console.log(`Part 2: ${}`);
-
-// 1163751742 2274862853 3385973964 4496184175 5517195186
-// 1163751742 2274862853 3385973964 4496184175 5517295286
+console.log(`Part 1: ${gridShortestPath(input)}`);
+console.log(`Part 2: ${gridShortestPath(bigGrid)}`);
