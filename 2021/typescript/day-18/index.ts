4a94aa68deb339e7cd377a592f95366b3f491b2f
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2021-12-19 00:03:15 +0000

    Try different approach (still not working)

--- 2021/typescript/day-18/index.ts
+++ 2021/typescript/day-18/index.ts
@@ -5,8 +5,7 @@
 import { readInput } from '../utils';
 
 // TYPES
-type Element = Pair | number;
-type NestedArray = Array<number | NestedArray>;
+type Snailfish = number | Array<Snailfish>;
 
 // INPUTS
 const snailfish = readInput('./../../inputs/day-17.txt');
@@ -18,88 +17,78 @@ const test = `[1,1]
   .map((e) => JSON.parse(e));
 
 // UTILS
-class Pair {
-  left: Element;
-  right: Element;
-  parent: Pair | null;
-  nesting: number;
-
-  constructor(left: Element, right: Element, parent?: Pair) {
-    this.left = left;
-    this.right = right;
-    this.parent = parent || null;
-    this.nesting = parent ? parent.nesting + 1 : 0;
-    this.setParent();
-    this.increaseNesting();
-  }
-
-  increaseNesting() {
-    if (this.left instanceof Pair) {
-      (this.left as Pair).nesting += 1;
-      if (this.left.nesting === 4) this.left.explode();
-      else (this.left as Pair).increaseNesting();
-    }
-    if (this.right instanceof Pair) {
-      (this.right as Pair).nesting += 1;
-      if (this.right.nesting === 4) this.right.explode();
-      else (this.right as Pair).increaseNesting();
-    }
-  }
-
-  setParent() {
-    if (this.left instanceof Pair) this.left.parent = this;
-    if (this.right instanceof Pair) this.right.parent = this;
-  }
-
-  explode() {
-    let lTarget = this.parent;
-    let rTarget = this.parent;
-    while (lTarget instanceof Pair && lTarget.left instanceof Pair)
-      lTarget = lTarget.parent as Pair;
-    while (rTarget instanceof Pair && rTarget.right instanceof Pair) {
-      rTarget = rTarget.parent as Pair;
-    }
-    if (lTarget) (lTarget.left as number) += this.left as number;
-    if (rTarget) (rTarget.right as number) += this.right as number;
-    if (this.parent?.left === this) this.parent.left = 0;
-    else if (this.parent?.right === this) this.parent.right = 0;
-  }
-
-  split() {}
-
-  toString(): string {
-    return `[${this.left instanceof Pair ? this.left.toString() : this.left}, ${
-      this.right instanceof Pair ? this.right.toString() : this.right
-    }]`;
-  }
-}
-
-const parseSnailfish = (arr: NestedArray): Pair => {
-  const [left, right] = arr;
-  let leftElement, rightElement;
-  if (Array.isArray(left)) {
-    leftElement = parseSnailfish(left);
-  }
-  if (Array.isArray(right)) {
-    rightElement = parseSnailfish(right);
-  }
-  return new Pair(
-    leftElement || (left as number),
-    rightElement || (right as number)
-  );
+const explode = (num: Snailfish, depth = 0): { didExplode: boolean; val: Snailfish } => {
+  // console.log(JSON.stringify(num), depth);
+  // if (typeof num === 'number') return { didExplode: false, val: num };
+  // else {
+  //   const { leftExplode: didExplode, leftVal: val } = explode(num[0], depth + 1);
+  //   if (leftExplode)
+  // }
 };
 
-const add = (a: Pair, b: Pair): Pair => new Pair(a, b);
+/**
+ * Performs a single split on input number. Also returns whether any numbers were split
+ * or not.
+ * @param {Snailfish} num - Number or nested array of numbers to try and split
+ * @returns {Object} Object containing `didSplit` boolean (true if passed value was split)
+ * and the value itself
+ */
+const split = (num: Snailfish): { didSplit: boolean; val: Snailfish } => {
+  if (typeof num === 'number') {
+    // If is a number, split it and return new array. Otherwise, keep old number
+    if (num >= 10) return { didSplit: true, val: [Math.floor(num / 2), Math.ceil(num / 2)] };
+    return { didSplit: false, val: num };
+  } else {
+    // If is an array, try split left element first, if split, return it
+    const { didSplit: leftSplit, val: leftVal } = split(num[0]);
+    if (leftSplit) return { didSplit: true, val: [leftVal, num[1]] };
+    else {
+      // If left element couldn't be split, try splitting right element
+      const { didSplit: rightSplit, val: rightVal } = split(num[0]);
+      return { didSplit: rightSplit, val: [leftVal, rightVal] };
+    }
+  }
+};
+
+/**
+ * Adds two snailfish numbers together and reduces their value if possible
+ * @param {Snailfish} a - First snailfish number
+ * @param {Snailfish} b - Second snailfish number
+ * @returns {Snailfish} The reduced sum of the two numbers
+ */
+const add = (a: Snailfish, b: Snailfish): Snailfish => reduce([a, b]);
+
+/**
+ * Recursively reduces a snailfish number by trying to explode it and then by trying
+ * to split it.
+ * @param {Snailfish} num - Snailfish number to reduce
+ * @returns {Snailfish} The reduced number
+ */
+const reduce = (num: Snailfish): Snailfish => {
+  let didExplode, didSplit, val;
+  // Try to explode the number. If exploded, reduce it again
+  ({ didExplode, val } = explode(num));
+  if (didExplode) return reduce(val);
+  // Try to spliut the number. If split, reduce it again
+  ({ didSplit, val } = split(num));
+  if (didSplit) return reduce(val);
+  // If neither exploded or split, return the number
+  return val;
+};
 
 // PART 1
+/**
+ * Finds the total magnitude of a snailfish number
+ * @param {Snailfish} num - A snailfish number
+ * @returns {number} The magnitude
+ */
+const magnitude = (num: Snailfish): number => {
+  if (typeof num === 'number') return num;
+  return 3 * magnitude(num[0]) + 2 * magnitude(num[1]);
+};
 
 // PART 2
 
 // OUTPUTS
-const a = parseSnailfish([
-  [3, [2, [1, [7, 3]]]],
-  [6, [5, [4, [3, 2]]]],
-]);
-console.log(a.toString());
 // console.log(`Part 1: ${}`);
 // console.log(`Part 2: ${}`);
