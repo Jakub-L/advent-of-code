7b9ae13e18f31fb117b7472273d59a94548dc0d5
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-23 15:20:22 +0100

    Add documentation

--- 2021/typescript/day-20/index.ts
+++ 2021/typescript/day-20/index.ts
@@ -9,11 +9,16 @@ import { readInput } from '../utils';
 // INPUTS
 const [lookup, rawImage] = readInput('./../../inputs/day-20.txt', '\n\n');
 let image = rawImage.split('\n');
-// let lookup =
-//   '..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#';
-// let image = ['#..#.', '#....', '##..#', '..#..', '..###'];
 
 // UTILS
+/**
+ * Finds the index of the new, enhanced pixel
+ * @param {number} i - Row index of target pixel
+ * @param {number} j - Column index of target pixel
+ * @param {string[]} image - Array of strings representing the previous image iteration
+ * @param {string} infVal - The value a pixel should take at infinity
+ * @returns {number} - Decimal index of the new pixel value
+ */
 const getIndex = (i: number, j: number, image: string[], infVal: string): number =>
   parseInt(
     [
@@ -34,6 +39,13 @@ const getIndex = (i: number, j: number, image: string[], infVal: string): number
     2
   );
 
+/**
+ * Performs a single image enhancement pass on the specified image
+ * @param {string} lookup - The image enhancement algorithm lookup string
+ * @param {string[]} image - Array of strings representing the previous image iteration
+ * @param {number} iteration - Which expansion it is. Used for looking up value at infinity
+ * @returns {string[]} The new, enhanced image
+ */
 const expandOnce = (lookup: string, image: string[], iteration: number): string[] => {
   const infVal = iteration % 2 ? lookup[lookup.length - 1] : lookup[0];
   // const infVal = "."
@@ -50,6 +62,13 @@ const expandOnce = (lookup: string, image: string[], iteration: number): string[
   return newImage;
 };
 
+/**
+ * Performs any number of the image enhancement algorithm. 
+ * @param {string} lookup - The image enhancement algorithm lookup string
+ * @param {string[]} image - Array of strings representing the input image
+ * @param {number} iterations - How many iterations of the algorithm should take place
+ * @returns {string[]} Enhanced image as a new array
+ */
 const expandImage = (lookup: string, image: string[], iterations: number) => {
   let newImage = Array.from(image);
   for (let iter = 0; iter < iterations; iter++) {
@@ -58,7 +77,12 @@ const expandImage = (lookup: string, image: string[], iterations: number) => {
   return newImage;
 };
 
-const countLit = (image: string[]) =>
+/**
+ * Counts the number of lit pixels in an image
+ * @param {string[]} image - Array of strings representing the image
+ * @returns {number} Number of lit pixels
+ */
+const countLit = (image: string[]): number =>
   image.reduce((acc, row) => acc + row.split('').filter((char) => char === '#').length, 0);
 
 // PART 1
@@ -69,7 +93,5 @@ console.log(countLit(firstImage));
 const secondImage = expandImage(lookup, image, 50);
 
 // OUTPUTS
-
 console.log(`Part 1: ${countLit(firstImage)}`);
 console.log(`Part 2: ${countLit(secondImage)}`);
-console.log(secondImage.join('\n'));
