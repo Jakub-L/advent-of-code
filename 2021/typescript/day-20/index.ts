aafd61d0bc656fefb857585e80bb5299e10bc987
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-23 15:09:33 +0100

    Solve day 20 challenge

--- 2021/typescript/day-20/index.ts
+++ 2021/typescript/day-20/index.ts
@@ -14,7 +14,7 @@ let image = rawImage.split('\n');
 // let image = ['#..#.', '#....', '##..#', '..#..', '..###'];
 
 // UTILS
-const getIndex = (i: number, j: number, image: string[]) =>
+const getIndex = (i: number, j: number, image: string[], infVal: string): number =>
   parseInt(
     [
       [-1, -1],
@@ -26,16 +26,23 @@ const getIndex = (i: number, j: number, image: string[]) =>
       [1, -1],
       [1, 0],
       [1, 1],
-    ].reduce((acc, [di, dj]) => `${acc}${image[i + di]?.[j + dj] === '#' ? 1 : 0}`, ''),
+    ].reduce((acc, [di, dj]) => {
+      if (image[i + di]?.[j + dj] === '#') return `${acc}1`;
+      else if (image[i + di]?.[j + dj] === '.') return `${acc}0`;
+      else return `${acc}${infVal === '#' ? 0 : 1}`;
+    }, ''),
     2
   );
 
-const expandImage = (lookup: string, image: string[]) => {
+const expandOnce = (lookup: string, image: string[], iteration: number): string[] => {
+  const infVal = iteration % 2 ? lookup[lookup.length - 1] : lookup[0];
+  // const infVal = "."
+  console.log(infVal);
   const newImage = [];
   for (let i = -1; i <= image.length; i++) {
     let row = '';
     for (let j = -1; j <= image[0].length; j++) {
-      const index = getIndex(i, j, image);
+      const index = getIndex(i, j, image, infVal);
       row = `${row}${lookup[index]}`;
     }
     newImage.push(row);
@@ -43,21 +50,26 @@ const expandImage = (lookup: string, image: string[]) => {
   return newImage;
 };
 
+const expandImage = (lookup: string, image: string[], iterations: number) => {
+  let newImage = Array.from(image);
+  for (let iter = 0; iter < iterations; iter++) {
+    newImage = expandOnce(lookup, newImage, iter);
+  }
+  return newImage;
+};
+
 const countLit = (image: string[]) =>
   image.reduce((acc, row) => acc + row.split('').filter((char) => char === '#').length, 0);
+
 // PART 1
+const firstImage = expandImage(lookup, image, 2);
+console.log(countLit(firstImage));
 
 // PART 2
+const secondImage = expandImage(lookup, image, 50);
 
 // OUTPUTS
-console.log(image.length, image[0].length)
-image = expandImage(lookup, image);
-console.log(image.length, image[0].length)
-// image = expandImage(lookup, image);
-console.log(countLit(image))
 
-// console.log(image);
-
-// console.log(draw(expandImage(lookup, image)));
-// console.log(`Part 1: ${}`);
-// console.log(`Part 2: ${}`);
+console.log(`Part 1: ${countLit(firstImage)}`);
+console.log(`Part 2: ${countLit(secondImage)}`);
+console.log(secondImage.join('\n'));
