9176184bce897c4e5259a66fee930070e439abf8
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-24 14:09:40 +0100

    Solve part 2 of day 21

--- 2021/typescript/day-21/index.ts
+++ 2021/typescript/day-21/index.ts
@@ -32,11 +32,11 @@ function* deterministicDie(): Generator {
  * to Part 1 of the day's challenge
  * @param {number[]} start - The starting positions of both players
  * @param {number} win - The score needed for a termination of the game. Must be exceeed or
- *                       equalled. 
+ *                       equalled.
  * @returns {number} The product of the losing player's score and the number of dice rolls
  *                   needed for a win
  */
-const deterministicGame = (start: number[], win: number): number => {
+const deterministicGame = (start: number[], win: number = 1000): number => {
   const die = deterministicDie();
   let [p1, p2] = start;
   let [s1, s2] = [0, 0];
@@ -49,7 +49,43 @@ const deterministicGame = (start: number[], win: number): number => {
 };
 
 // PART 2
+/**
+ * Plays a quantum game using a quantum dice and finds the number of wins for each player
+ * @param {number[]} position - Positions of the two players on the board
+ * @param {number[]} [score=[0,0]] - The current score of the two players
+ * @param {Object} [cache={}] - The cached positions/scores and the number of wins they lead to
+ * @param {number} [win=21] - The score needed for a win
+ * @returns {number[]} Number of universes where, from the current position/score, each of
+ *                     players wins
+ */
+const quantumGame = (
+  position: number[],
+  score: number[] = [0, 0],
+  cache: { [index: string]: number[] } = {},
+  win: number = 21
+): number[] => {
+  // prettier-ignore
+  const moveCounts = [[3, 1], [4, 3], [5, 6], [6, 7], [7, 6], [8, 3], [9, 1]]
+  const key = `${position.join(' ')} ${score.join(' ')}`;
+  if (score[0] >= win) return [1, 0];
+  if (score[1] >= win) return [0, 1];
+  if (!(key in cache)) {
+    console.log(key)
+    let [w1, w2] = [0, 0];
+    const [s1, s2] = score;
+    const [p1, p2] = position;
+    for (let [move, num] of moveCounts) {
+      const newP1 = (p1 + move) % 10 || 10;
+      // Swap the players to mimic the other player taking a move
+      const [dW2, dW1] = quantumGame([p2, newP1], [s2, s1 + newP1], cache);
+      w1 += num * dW1;
+      w2 += num * dW2;
+    }
+    cache[key] = [w1, w2];
+  }
+  return cache[key];
+};
 
 // OUTPUTS
-console.log(`Part 1: ${deterministicGame(start, 1000)}`);
-// console.log(`Part 2: ${}`);
+console.log(`Part 1: ${deterministicGame(start)}`);
+console.log(`Part 2: ${Math.max(...quantumGame(start))}`);
