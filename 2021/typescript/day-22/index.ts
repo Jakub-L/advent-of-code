c0e167f1ef59e75b519fb11641a4de9a5950ae6a
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-24 14:48:58 +0100

    Add day 22 template and input

---
+++ 2021/typescript/day-22/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Solution to Day 22 challenge of Advent of Code 2021
+ * https://adventofcode.com/2021/day/22
+ */
+import { readInput } from '../utils';
+
+// TYPES
+
+// INPUTS
+const instructions = readInput('./../../inputs/day-22.txt');
+
+// UTILS
+
+// PART 1
+
+// PART 2
+
+// OUTPUTS
+// console.log(`Part 1: ${}`);
+// console.log(`Part 2: ${}`);
