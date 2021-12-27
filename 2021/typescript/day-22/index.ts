a3844a90529299c33dae9002adea9940528f4328
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-27 15:13:58 +0100

    Solve day 22 part 2

--- 2021/typescript/day-22/index.ts
+++ 2021/typescript/day-22/index.ts
@@ -19,31 +19,7 @@ type Instruction = {
 };
 
 // INPUTS
-const instructions = readInput('./../../inputs/day-22.txt').map(parseInstruction);
-const t = [
-  'on x=-20..26,y=-36..17,z=-47..7',
-  'on x=-20..33,y=-21..23,z=-26..28',
-  'on x=-22..28,y=-29..23,z=-38..16',
-  'on x=-46..7,y=-6..46,z=-50..-1',
-  'on x=-49..1,y=-3..46,z=-24..28',
-  'on x=2..47,y=-22..22,z=-23..27',
-  'on x=-27..23,y=-28..26,z=-21..29',
-  'on x=-39..5,y=-6..47,z=-3..44',
-  'on x=-30..21,y=-8..43,z=-13..34',
-  'on x=-22..26,y=-27..20,z=-29..19',
-  'off x=-48..-32,y=26..41,z=-47..-37',
-  'on x=-12..35,y=6..50,z=-50..-2',
-  'off x=-48..-32,y=-32..-16,z=-15..-5',
-  'on x=-18..26,y=-33..15,z=-7..46',
-  'off x=-40..-22,y=-38..-28,z=23..41',
-  'on x=-16..35,y=-41..10,z=-47..6',
-  'off x=-32..-23,y=11..30,z=-14..3',
-  'on x=-49..-5,y=-3..45,z=-29..18',
-  'off x=18..30,y=-20..-8,z=-3..13',
-  'on x=-41..9,y=-7..43,z=-33..15',
-  'on x=-54112..-39298,y=-85059..-49293,z=-27449..7877',
-  'on x=967..23432,y=45373..81175,z=27513..53682',
-].map(parseInstruction);
+const instructions = readInput('./../../inputs/day-22.txt').map(parseInstruction).reverse();
 
 // UTILS
 function parseInstruction(instruction: string): Instruction {
@@ -112,8 +88,7 @@ const findOverlapsVolume = (target: Cuboid, compared: Cuboid[]): number => {
 // PART 1
 let part1On = 0;
 const part1Cubes: Cuboid[] = [];
-
-for (let { operation, dimensions } of instructions.reverse()) {
+for (let { operation, dimensions } of instructions) {
   const cuboid = new Cuboid(dimensions.x, dimensions.y, dimensions.z);
   if (
     cuboid.x.min >= -50 &&
@@ -133,12 +108,9 @@ for (let { operation, dimensions } of instructions.reverse()) {
 // PART 2
 let part2On = 0;
 const part2Cubes: Cuboid[] = [];
-
-for (let { operation, dimensions } of instructions.reverse()) {
+for (let { operation, dimensions } of instructions) {
   const cuboid = new Cuboid(dimensions.x, dimensions.y, dimensions.z);
-  if (operation === 'on') {
-    part2On += cuboid.volume - findOverlapsVolume(cuboid, part2Cubes);
-  }
+  if (operation === 'on') part2On += cuboid.volume - findOverlapsVolume(cuboid, part2Cubes);
   part2Cubes.push(cuboid);
 }
 
