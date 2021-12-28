e0f591ec45ebac10bea6f2b91328f7fc45f3bfb6
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-12-28 22:38:22 +0100

    "Solve" day 23

---
+++ 2021/typescript/day-23/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Solution to Day 23 challenge of Advent of Code 2021
+ * https://adventofcode.com/2021/day/23
+ */
+
+/*
+ * There is no code solution to this day's challenge.
+ * The correct solution can be obtained manually with a bit of
+ * trial and error.
+ * 
+ * I used a tool someone on Reddit made to assist in solving
+ * this puzzle: https://amphipod.net/
+ */
