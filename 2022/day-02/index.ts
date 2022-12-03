2b2f1426139dcc5f040730eb3b3c9f93aed39429
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2022-12-03 14:54:55 +0000

    Correct day in comments

--- 2022/day-02/index.ts
+++ 2022/day-02/index.ts
@@ -1,6 +1,6 @@
 /**
- * Solution to Day 1 challenge of Advent of Code 2022
- * https://adventofcode.com/2022/day/1
+ * Solution to Day 2 challenge of Advent of Code 2022
+ * https://adventofcode.com/2022/day/2
  */
 import { readInput } from '../utils';
 
