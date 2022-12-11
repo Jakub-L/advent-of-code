95751a4acc680de5e2a17f70fa3e12b2052751ed
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2022-12-11 18:14:48 +0000

    Solve day 11 part 2

--- 2022/day-11/index.ts
+++ 2022/day-11/index.ts
@@ -1,71 +1,62 @@
 /**
  * Solution to Day 11 challenge of Advent of Code 2022
  * https://adventofcode.com/2022/day/11
+ * 
+ * 
+ * Part 2 of this puzzle uses this identity:
+ *    a mod n = (a mod k*n) mod n; where k is any positive integer.
+ * 
+ * What this means is rather than looking at (worry mod n) for each monkey's test, we 
+ * can instead look at (worry mod k*n) mod n and the result will be the same. At the 
+ * same time, this allowsus to keep the stored value (worry mod k*n) smaller than 
+ * just (worry mod n).
+ * 
+ * Obviously, (k*n) has to be a number that divides n. If we use k*n such that it divides
+ * every monkey's modulus check, we can just store (worry mod k*n). The simplest way
+ * to find (k*n) such that it divides all monkeys' moduli is to multiply them together.
  */
 
 // INPUTS
-// const monkeys: MonkeyDefinition[] = [
-//   {
-//     startingItems: [79, 98],
-//     operation: old => old * 19,
-//     testDefinition: { conditionModulus: 23, trueIndex: 2, falseIndex: 3 }
-//   },
-//   {
-//     startingItems: [54, 65, 75, 74],
-//     operation: old => old + 6,
-//     testDefinition: { conditionModulus: 19, trueIndex: 2, falseIndex: 0 }
-//   },
-//   {
-//     startingItems: [79, 60, 97],
-//     operation: old => old ** 2,
-//     testDefinition: { conditionModulus: 13, trueIndex: 1, falseIndex: 3 }
-//   },
-//   {
-//     startingItems: [74],
-//     operation: old => old + 3,
-//     testDefinition: { conditionModulus: 17, trueIndex: 0, falseIndex: 1 }
-//   }
-// ];
 const monkeys: MonkeyDefinition[] = [
   {
     startingItems: [65, 58, 93, 57, 66],
     operation: old => old * 7,
-    testDefinition: { conditionModulus: 19, trueIndex: 6, falseIndex: 4 }
+    testDef: { mod: 19, trueIndex: 6, falseIndex: 4 }
   },
   {
     startingItems: [76, 97, 58, 72, 57, 92, 82],
     operation: old => old + 4,
-    testDefinition: { conditionModulus: 3, trueIndex: 7, falseIndex: 5 }
+    testDef: { mod: 3, trueIndex: 7, falseIndex: 5 }
   },
   {
     startingItems: [90, 89, 96],
     operation: old => old * 5,
-    testDefinition: { conditionModulus: 13, trueIndex: 5, falseIndex: 1 }
+    testDef: { mod: 13, trueIndex: 5, falseIndex: 1 }
   },
   {
     startingItems: [72, 63, 72, 99],
     operation: old => old * old,
-    testDefinition: { conditionModulus: 17, trueIndex: 0, falseIndex: 4 }
+    testDef: { mod: 17, trueIndex: 0, falseIndex: 4 }
   },
   {
     startingItems: [65],
     operation: old => old + 1,
-    testDefinition: { conditionModulus: 2, trueIndex: 6, falseIndex: 2 }
+    testDef: { mod: 2, trueIndex: 6, falseIndex: 2 }
   },
   {
     startingItems: [97, 71],
     operation: old => old + 8,
-    testDefinition: { conditionModulus: 11, trueIndex: 7, falseIndex: 3 }
+    testDef: { mod: 11, trueIndex: 7, falseIndex: 3 }
   },
   {
     startingItems: [83, 68, 88, 55, 87, 67],
     operation: old => old + 2,
-    testDefinition: { conditionModulus: 5, trueIndex: 2, falseIndex: 1 }
+    testDef: { mod: 5, trueIndex: 2, falseIndex: 1 }
   },
   {
     startingItems: [64, 81, 50, 96, 82, 53, 62, 92],
     operation: old => old + 5,
-    testDefinition: { conditionModulus: 7, trueIndex: 3, falseIndex: 0 }
+    testDef: { mod: 7, trueIndex: 3, falseIndex: 0 }
   }
 ];
 
@@ -73,14 +64,14 @@ const monkeys: MonkeyDefinition[] = [
 type Operation = (old: number) => number;
 type TestFunction = (worry: number) => Monkey;
 type TestDefinition = {
-  conditionModulus: number;
+  mod: number;
   trueIndex: number;
   falseIndex: number;
 };
 type MonkeyDefinition = {
   startingItems: number[];
   operation: Operation;
-  testDefinition: TestDefinition;
+  testDef: TestDefinition;
 };
 
 /** A single monkey */
@@ -95,17 +86,21 @@ class Monkey {
    * @param {Number} startingItems - Worry levels for the inital items held by the monkey
    * @param {Operation} operation - Function defining how inspecting the item affects the
    *      worry levels
+   * @param {boolean} dividesBy3 - Whether the worry level is divided by 3 after every inspection
    */
   constructor(startingItems: number[], operation: Operation) {
-    this.items = startingItems;
+    this.items = Array.from(startingItems);
     this.operation = operation;
   }
 
   /** Processes all items in a monkey's collection */
-  processItems() {
+  processItems(modulus: number = 0) {
     while (this.items.length > 0) {
       const oldWorry = this.items.shift() || 0;
-      const newWorry = Math.floor(this.operation(oldWorry) / 3);
+      const newWorry =
+        modulus === 0
+          ? Math.floor(this.operation(oldWorry) / 3)
+          : this.operation(oldWorry) % modulus;
       const targetMonkey = this.test(newWorry);
       targetMonkey.items.push(newWorry);
       this.inspectedItems += 1;
@@ -116,19 +111,23 @@ class Monkey {
 /** A pack of monkeys */
 class Pack {
   private monkeys: Monkey[] = [];
+  private modulus: number = 0;
 
   /**
    * Creates a new pack of monkeys
    * @param {MonkeyDefinition[]} monkeys - Definitions of all the monkeys
+   * @param {boolean} [divideBy3=true] - Whether the worry level is divided by three
+   *    after every monkey's inspection
    */
-  constructor(monkeys: MonkeyDefinition[]) {
+  constructor(monkeys: MonkeyDefinition[], divideBy3: boolean = true) {
     for (const monkey of monkeys) {
       this.monkeys.push(new Monkey(monkey.startingItems, monkey.operation));
     }
     // Once the monkeys have been created, the pointers can be created
     for (let i = 0; i < this.monkeys.length; i++) {
-      this.monkeys[i].test = this.parseTest(monkeys[i].testDefinition);
+      this.monkeys[i].test = this.parseTest(monkeys[i].testDef);
     }
+    if (!divideBy3) this.modulus = monkeys.reduce((acc, m) => acc * m.testDef.mod, 1);
   }
 
   /**
@@ -137,7 +136,7 @@ class Pack {
    */
   playRounds(count: number) {
     for (let n = 0; n < count; n++) {
-      for (const monkey of this.monkeys) monkey.processItems();
+      for (const monkey of this.monkeys) monkey.processItems(this.modulus);
     }
   }
 
@@ -158,16 +157,19 @@ class Pack {
   private parseTest(def: TestDefinition): TestFunction {
     const monkeys = this.monkeys;
     return function (worry: number): Monkey {
-      return worry % def.conditionModulus === 0
-        ? monkeys[def.trueIndex]
-        : monkeys[def.falseIndex];
+      return worry % def.mod === 0 ? monkeys[def.trueIndex] : monkeys[def.falseIndex];
     };
   }
 }
 
 // PART 1
-const pack = new Pack(monkeys);
-pack.playRounds(20);
+const firstPack = new Pack(monkeys);
+firstPack.playRounds(20);
 
-// RESULTS
-console.log(`Part 1 solution: ${pack.monkeyBusiness}`);
+// PART 2
+const secondPack = new Pack(monkeys, false);
+secondPack.playRounds(10000);
+
+// // RESULTS
+console.log(`Part 1 solution: ${firstPack.monkeyBusiness}`);
+console.log(`Part 2 solution: ${secondPack.monkeyBusiness}`);
