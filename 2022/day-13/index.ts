081e6ca0d5b29f4bc2079a0a2d0b6f42095e1ce6
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2022-12-14 20:04:58 +0000

    Solve day 13 challenge

--- 2022/day-13/index.ts
+++ 2022/day-13/index.ts
@@ -5,61 +5,58 @@
 import { readInput } from '../utils';
 
 // INPUTS
-// const signals: string[] = readInput('./day-13/input.txt', '\n\n');
-const signals: Packet[][] = `[1,1,3,1,1]
-[1,1,5,1,1]
-
-[[1],[2,3,4]]
-[[1],4]
-
-[9]
-[[8,7,6]]
-
-[[4,4],4,4]
-[[4,4],4,4,4]
-
-[7,7,7,7]
-[7,7,7]
-
-[]
-[3]
-
-[[[]]]
-[[]]
-
-[1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]`
-  .split('\n\n')
-  .map(pair => pair.split('\n').map(str => JSON.parse(str)));
+const packetsAsPairs: Packet[][] = readInput('./day-13/input.txt', '\n\n').map(pair =>
+  pair.split('\n').map(str => JSON.parse(str))
+);
+const packetsAsList: Packet[][] = readInput('./day-13/input.txt')
+  .filter(row => row.length !== 0)
+  .map(str => JSON.parse(str))
+  .concat([[[2]], [[6]]]);
 
 // UTILS
 type Packet = Array<number | Packet>;
 
-const toArrayIfNeeded = (elem: number | Packet): number[] | Packet =>
+/**
+ * Wraps a number into an array, if it isn't an array already
+ * @param {number | Packet} elem - Element to wrap
+ * @returns {Packet} The input in the form of a Packet
+ */
+const toPacket = (elem: number | Packet): number[] | Packet =>
   typeof elem === 'number' ? [elem] : elem;
 
-// PART 1
-const isInRightOrder = (pair: Packet[]): boolean | null => {
-  const compareElements = (a: number | Packet, b: number | Packet): boolean | null => {
+/**
+ * Compares a pair of packets to see if they are in the correct order
+ * @param {Packet[]} pair - A pair of packets to compare
+ * @returns {boolean | null} True if in correct order, false if in incorrect,
+ *    null if unable to determine
+ */
+const isInRightOrder = ([first, second]: Packet[]): boolean | null => {
+  for (let i = 0; i < Math.min(first.length, second.length); i++) {
+    const [a, b] = [first[i], second[i]];
     if (typeof a === 'number' && typeof b === 'number') {
-      if (a === b) return null;
+      if (a === b) continue;
       return a < b;
     }
-    return isInRightOrder([toArrayIfNeeded(a), toArrayIfNeeded(b)]);
-  };
-
-  const [first, second] = pair;
-  for (let i = 0; i < Math.min(first.length, second.length); i++) {
-    const result = compareElements(first[i], second[i]);
+    const result = isInRightOrder([toPacket(a), toPacket(b)]);
     if (result === null) continue;
     return result;
   }
-  return first.length < second.length; // If left side ran out first, inputs are in the right order
+  if (first.length === second.length) return null;
+  return first.length < second.length;
 };
 
-console.log(
-  isInRightOrder([
-    [[1], [2, 3, 4]],
-    [[1], 4]
-  ])
+// PART 1
+const sumOfCorrectIndices = packetsAsPairs.reduce(
+  (sum, pair, i) => sum + (isInRightOrder(pair) ? i + 1 : 0),
+  0
 );
+
+// PART 2
+const decoderKey = packetsAsList
+  .sort((a, b) => (isInRightOrder([a, b]) ? -1 : 1))
+  .map(packets => JSON.stringify(packets))
+  .reduce((acc, packet, i) => acc * (['[[2]]', '[[6]]'].includes(packet) ? i + 1 : 1), 1);
+
+// RESULT
+console.log(`Part 1 solution: ${sumOfCorrectIndices}`);
+console.log(`Part 2 solution: ${decoderKey}`);
