73d7ba8a701069b9d2f149671cef1919e850b3b4
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2022-12-22 02:03:54 +0000

    Solve day 19 part 2

--- 2022/day-19/index.ts
+++ 2022/day-19/index.ts
@@ -1,216 +1,222 @@
 /**
  * Solution to Day 19 challenge of Advent of Code 2022
  * https://adventofcode.com/2022/day/19
+ *
+ * I couldn't get my original method to work fast enough. This method is based on the code
+ * of tymscar: [https://github.com/tymscar/Advent-Of-Code]
  */
 import { readInput } from '../utils';
 
 // INPUTS
-const robotCosts: string[] = readInput('./day-19/input.txt');
+const blueprints: Blueprint[] = readInput('./day-19/input.txt').map(parseBlueprintString);
 
 // UTILS
-type Resources = { ore: number; clay: number; obsidian: number; geodes: number };
-type Robots = Resources;
+type Materials = {
+  ore: number;
+  clay: number;
+  obsidian: number;
+  geode: number;
+};
+type RobotCost = Materials;
+type RobotsCount = Materials;
+type Blueprint = {
+  id: number;
+  oreRobot: RobotCost;
+  clayRobot: RobotCost;
+  obsidianRobot: RobotCost;
+  geodeRobot: RobotCost;
+};
 
-class RobotCosts {
-  oreRobot: Resources = { ore: 0, clay: 0, obsidian: 0, geodes: 0 };
-  clayRobot: Resources = { ore: 0, clay: 0, obsidian: 0, geodes: 0 };
-  obsidianRobot: Resources = { ore: 0, clay: 0, obsidian: 0, geodes: 0 };
-  geodeRobot: Resources = { ore: 0, clay: 0, obsidian: 0, geodes: 0 };
-  mostExpensive: Resources;
-
-  constructor(blueprintString: string) {
-    const parsed = this._parseString(blueprintString);
-    this.oreRobot = parsed.ore;
-    this.clayRobot = parsed.clay;
-    this.obsidianRobot = parsed.obsidian;
-    this.geodeRobot = parsed.geode;
-    this.mostExpensive = this._getMostExpensive();
+/**
+ * Processes a blueprint string into a Blueprint object
+ * @param {string} str - Raw blueprint string to parse
+ * @param {number} index - Index of the blueprint in its array
+ * @returns {Blueprint} A processed Blueprint object
+ */
+function parseBlueprintString(str: string, index: number): Blueprint {
+  const regex = /(\w+) robot costs (\d+ ore)(?: and (\d+ (?:clay|obsidian)))?/g;
+  const result: { [index: string]: Object } = { id: index + 1 };
+  for (const [_, type, ...costs] of str.matchAll(regex)) {
+    result[`${type}Robot`] = costs
+      .filter(e => e)
+      .reduce(
+        (acc, cost) => {
+          const [val, ore] = cost.split(' ');
+          return { ...acc, [ore]: Number(val) };
+        },
+        { ore: 0, clay: 0, obsidian: 0, geode: 0 }
+      );
   }
+  return result as Blueprint;
+}
 
-  private _getMostExpensive(): Resources {
-    return this._robots.reduce(
-      (acc, robot) => {
-        let type: keyof Resources;
-        for (type in robot) {
-          acc[type] = Math.max(acc[type], robot[type]);
-        }
-        return acc;
-      },
-      { ore: 0, clay: 0, obsidian: 0, geodes: 0 }
-    );
-  }
+/**
+ * Calculates the maximum number of geodes that could be mined in a specified time, given
+ * specific costs of each mining robot.
+ * @param {number} time - Number of minutes remaining
+ * @param {Blueprint} blueprint - Blueprint object defining the robot costs
+ * @param {RobotsCount} [robots={ore:1, clay:0, obsidian:0, geode:0}] - Counts of mining robots
+ * @param {Materials} [materials={ore:0, clay:0, obsidian:0, geode:0}] - Materials available for construction
+ * @param {number} [currMax=0] - The current maximum known geode number
+ * @returns
+ */
+const getMaxGeodes = (
+  time: number,
+  blueprint: Blueprint,
+  robots: RobotsCount = { ore: 1, clay: 0, obsidian: 0, geode: 0 },
+  materials: Materials = { ore: 0, clay: 0, obsidian: 0, geode: 0 },
+  currMax: number = 0
+): number => {
+  /**
+   * Checks if a robot can be purchased.
+   * @param {string} type - Type of robot to buy
+   * @returns {boolean} True if robot can be afforded, false otherwise
+   */
+  const canBuyRobot = (type: string): boolean => {
+    const cost = blueprint[`${type}Robot` as keyof Blueprint];
+    for (const [mat, reqVal] of Object.entries(cost)) {
+      if (reqVal > materials[mat as keyof Materials]) return false;
+    }
+    return true;
+  };
 
-  private get _robots(): Resources[] {
-    return [this.oreRobot, this.clayRobot, this.obsidianRobot, this.geodeRobot];
-  }
-
-  private _parseString(blueprintString: string): { [index: string]: Resources } {
-    const regex = /(\w+) robot costs (\d+ ore)(?: and (\d+ (?:clay|obsidian)))?/g;
-    const result: { [index: string]: Resources } = {};
-    for (const [_, type, ...costs] of blueprintString.matchAll(regex)) {
-      result[type] = costs
-        .filter(e => e)
-        .reduce(
-          (acc, cost) => {
-            const [val, ore] = cost.split(' ');
-            return { ...acc, [ore]: Number(val) };
-          },
-          { ore: 0, clay: 0, obsidian: 0, geodes: 0 } as Resources
-        );
+  /**
+   * Calculates the number of minutes needed until all the resources for a particular robot are
+   * available, assuming no robots are purchased in the meantime.
+   * @param {string} type - Type of robot to buy
+   * @returns {number} The number of minutes needed to wait
+   */
+  const timeUntilEnough = (type: string): number => {
+    const cost = blueprint[`${type}Robot` as keyof Blueprint];
+    let result = -1;
+    for (const [mat, reqVal] of Object.entries(cost)) {
+      if (robots[mat as keyof RobotsCount] === 0) continue;
+      result = Math.max(
+        result,
+        Math.ceil(
+          (reqVal - materials[mat as keyof Materials]) / robots[mat as keyof RobotsCount]
+        )
+      );
     }
     return result;
+  };
+
+  /**
+   * Buys a single robot and then returns the new robots object
+   * @param {string} type - Type of robot to buy
+   * @returns {RobotsCount} Counts of robots after purchase
+   */
+  const buyRobot = (type: string): RobotsCount => {
+    return { ...robots, [type]: robots[type as keyof RobotsCount] + 1 };
+  };
+
+  if (time <= 0) return currMax;
+  currMax = Math.max(materials.geode, currMax);
+
+  const maxOreNeeded = Math.max(
+    blueprint.oreRobot.ore,
+    blueprint.clayRobot.ore,
+    blueprint.obsidianRobot.ore,
+    blueprint.geodeRobot.ore
+  );
+
+  if (robots.obsidian > 0) {
+    // See how long you'd need to wait for a geode robot, wait that long and then buy the robot.
+    const timeUntilEnoughResources = timeUntilEnough('geode');
+    const totalTime = 1 + (canBuyRobot('geode') ? 0 : timeUntilEnoughResources);
+
+    const newMaterials = {
+      ...materials,
+      ore: materials.ore + totalTime * robots.ore - blueprint.geodeRobot.ore,
+      clay: materials.clay + totalTime * robots.clay,
+      obsidian:
+        materials.obsidian + totalTime * robots.obsidian - blueprint.geodeRobot.obsidian,
+      geode: materials.geode + time - totalTime
+    };
+
+    currMax = Math.max(
+      getMaxGeodes(time - totalTime, blueprint, robots, newMaterials, currMax),
+      currMax
+    );
+
+    if (canBuyRobot('geode')) return currMax;
   }
-}
+  if (robots.clay > 0) {
+    // See how long you'd need to wait for an obsidian robot, wait that long and then buy the robot.
+    const timeUntilEnoughResources = timeUntilEnough('obsidian');
+    const totalTime = 1 + (canBuyRobot('obsidian') ? 0 : timeUntilEnoughResources);
 
-class Blueprint {
-  costs: RobotCosts;
-  maxTime: number;
-  private _id: number;
-  private _maxGeodes: number = -1;
+    if (time - totalTime > 2) {
+      const newMaterials = {
+        ...materials,
+        ore: materials.ore + totalTime * robots.ore - blueprint.obsidianRobot.ore,
+        clay: materials.clay + totalTime * robots.clay - blueprint.obsidianRobot.clay,
+        obsidian: materials.obsidian + totalTime * robots.obsidian
+      };
 
-  constructor(blueprintString: string, maxTime: number, id: number) {
-    this.costs = new RobotCosts(blueprintString);
-    this.maxTime = maxTime;
-    this._id = id;
-  }
-
-  get maxGeodes(): number {
-    if (this._maxGeodes < 0) this._maxGeodes = this._solve();
-    return this._maxGeodes;
-  }
-
-  get quality(): number {
-    if (this._maxGeodes < 0) this._maxGeodes = this._solve();
-    return this._maxGeodes * this._id;
-  }
-
-  private _solve() {
-    let best = 0;
-    const initialState: [Resources, Robots, number] = [
-      { ore: 0, clay: 0, obsidian: 0, geodes: 0 },
-      { ore: 1, clay: 0, obsidian: 0, geodes: 0 },
-      24
-    ];
-    let queue = [initialState];
-    const memo: Set<string> = new Set();
-
-    while (queue.length > 0) {
-      const state = queue.shift() as [Resources, Robots, number];
-      const [res, rob, t] = state;
-      best = Math.max(best, res.geodes);
-      if (t === 0) continue;
-
-      if (rob.ore >= this.costs.mostExpensive.ore) {
-        rob.ore = this.costs.mostExpensive.ore;
-      }
-      if (rob.clay >= this.costs.mostExpensive.clay) {
-        rob.clay = this.costs.mostExpensive.clay;
-      }
-      if (rob.obsidian >= this.costs.mostExpensive.obsidian) {
-        rob.obsidian = this.costs.mostExpensive.obsidian;
-      }
-
-      if (res.ore >= t * this.costs.mostExpensive.ore - rob.ore * (t - 1)) {
-        res.ore = t * this.costs.mostExpensive.ore - rob.ore * (t - 1);
-      }
-      if (res.clay >= t * this.costs.mostExpensive.clay - rob.clay * (t - 1)) {
-        res.clay = t * this.costs.mostExpensive.clay - rob.clay * (t - 1);
-      }
-      if (res.obsidian >= t * this.costs.mostExpensive.obsidian - rob.obsidian * (t - 1)) {
-        res.obsidian = t * this.costs.mostExpensive.obsidian - rob.obsidian * (t - 1);
-      }
-
-      const key = `${Object.values(res)},${Object.values(rob)},t`;
-      if (memo.has(key)) continue;
-      memo.add(key);
-
-      if (memo.size % 1000000 === 0) console.log(t, best, memo.size);
-      queue = queue.concat(this._getVariations(res, rob, t));
+      currMax = Math.max(
+        getMaxGeodes(time - totalTime, blueprint, buyRobot('obsidian'), newMaterials, currMax)
+      );
     }
-    return best;
   }
+  if (robots.clay < blueprint.obsidianRobot.clay) {
+    // See how long you'd need to wait for a clay robot, wait that long and then buy the robot.
+    const timeUntilEnoughOre = timeUntilEnough('clay');
+    const totalTime = 1 + (canBuyRobot('clay') ? 0 : timeUntilEnoughOre);
 
-  private _getVariations(res: Resources, rob: Robots, t: number) {
-    const result = [
-      [
-        {
-          ore: res.ore + rob.ore,
-          clay: res.clay + rob.clay,
-          obsidian: res.obsidian + rob.obsidian,
-          geodes: res.geodes + rob.geodes
-        },
-        { ...rob },
-        t - 1
-      ]
-    ];
-    if (res.ore >= this.costs.oreRobot.ore) {
-      result.push([
-        {
-          ore: res.ore + rob.ore - this.costs.oreRobot.ore,
-          clay: res.clay + rob.clay,
-          obsidian: res.obsidian + rob.obsidian,
-          geodes: res.geodes + rob.geodes
-        },
-        { ...rob, ore: rob.ore + 1 },
-        t - 1
-      ]);
+    if (time - totalTime > 3) {
+      const newMaterials = {
+        ...materials,
+        ore: materials.ore + totalTime * robots.ore - blueprint.clayRobot.ore,
+        clay: materials.clay + totalTime * robots.clay,
+        obsidian: materials.obsidian + totalTime * robots.obsidian
+      };
+
+      currMax = Math.max(
+        getMaxGeodes(time - totalTime, blueprint, buyRobot('clay'), newMaterials, currMax)
+      );
     }
-    if (res.ore >= this.costs.clayRobot.ore) {
-      result.push([
-        {
-          ore: res.ore + rob.ore - this.costs.clayRobot.ore,
-          clay: res.clay + rob.clay,
-          obsidian: res.obsidian + rob.obsidian,
-          geodes: res.geodes + rob.geodes
-        },
-        { ...rob, clay: rob.clay + 1 },
-        t - 1
-      ]);
-    }
-    if (res.ore >= this.costs.obsidianRobot.ore && res.clay >= this.costs.obsidianRobot.clay) {
-      result.push([
-        {
-          ore: res.ore + rob.ore - this.costs.obsidianRobot.ore,
-          clay: res.clay + rob.clay - this.costs.obsidianRobot.clay,
-          obsidian: res.obsidian + rob.obsidian,
-          geodes: res.geodes + rob.geodes
-        },
-        { ...rob, obsidian: rob.obsidian + 1 },
-        t - 1
-      ]);
-    }
-    if (
-      res.ore >= this.costs.geodeRobot.ore &&
-      res.obsidian >= this.costs.geodeRobot.obsidian
-    ) {
-      result.push([
-        {
-          ore: res.ore + rob.ore - this.costs.geodeRobot.ore,
-          clay: res.clay + rob.clay,
-          obsidian: res.obsidian + rob.obsidian - this.costs.geodeRobot.obsidian,
-          geodes: res.geodes + rob.geodes
-        },
-        { ...rob, geodes: rob.geodes + 1 },
-        t - 1
-      ]);
-    }
-    return result as [Resources, Robots, number][];
   }
-}
+  if (robots.ore < maxOreNeeded) {
+    // See how long you'd need to wait for an ore robot, wait that long and then buy the robot.
+    const timeUntilEnoughOre = timeUntilEnough('ore');
+    const totalTime = 1 + (canBuyRobot('ore') ? 0 : timeUntilEnoughOre);
+
+    // We need at least 5 seconds after this purchase to make any geodes:
+    // 1. Build ore robot
+    // 2. Build clay robot
+    // 3. Build obsidian robot
+    // 4. Build geode robot
+    // 5. Mine first geode
+    // If we don't have that time we might as well prune the path as we won't gain more geodes.
+    // This applies to all above paths as well
+    if (time - totalTime > 4) {
+      const newMaterials = {
+        ...materials,
+        ore: materials.ore + totalTime * robots.ore - blueprint.oreRobot.ore,
+        clay: materials.clay + totalTime * robots.clay,
+        obsidian: materials.obsidian + totalTime * robots.obsidian
+      };
+
+      currMax = Math.max(
+        getMaxGeodes(time - totalTime, blueprint, buyRobot('ore'), newMaterials, currMax)
+      );
+    }
+  }
+  return currMax;
+};
 
 // PART 1
-// let part1 = robotCosts.reduce((sum, rC, i) => {
-//   const blueprint = new Blueprint(rC, 24, i + 1);
-//   return sum + blueprint.quality;
-// }, 0);
+const part1 = blueprints.reduce(
+  (sum, blueprint) => sum + blueprint.id * getMaxGeodes(24, blueprint),
+  0
+);
 
 // PART 2
-let part2 = robotCosts.slice(0, 3).reduce((product, rC, i) => {
-  const blueprint = new Blueprint(rC, 32, i + 1);
-  console.log(blueprint.maxGeodes);
-  return product * blueprint.maxGeodes;
-}, 1);
+const part2 = blueprints
+  .slice(0, 3)
+  .reduce((prod, blueprint) => prod * getMaxGeodes(32, blueprint), 1);
 
 // RESULTS
-// console.log(`Part 1 solution: ${part1}`);
-console.log(`Part 2 solution: ${part2}`);
+console.log(`Solution to Part 1: ${part1}`);
+console.log(`Solution to Part 2: ${part2}`);
