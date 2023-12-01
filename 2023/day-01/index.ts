c55e10003ce9fdd6435078ed11628b4d4761097c
Jakub L <jakub.lech@sennen.tech>
2023-12-01 21:14:36 +0000

    Solve day 1, part 2

--- 2023/day-01/index.ts
+++ 2023/day-01/index.ts
@@ -3,13 +3,10 @@ import { sum } from "@jakub-l/aoc-lib/math";
 
 const input: string[] = readFile(__dirname + "/input.txt") as string[];
 
-
 /**
  * Calculates the calibration value based on the given string.
- * The calibration value is calculated by taking the first and last digit from the string and multiplying the first digit by 10, then adding the last digit.
- * If the string contains only one digit, the calibration value is calculated by multiplying the digit by 10 and adding the digit again.
- * @param str - The string from which to calculate the calibration value.
- * @returns The calibration value.
+ * @param {string} str - The string from which to calculate the calibration value.
+ * @returns {number} The calibration value.
  */
 const calibrationValue = (str: string) => {
   const numbers = str.match(/\d/g)?.map(Number) ?? [];
@@ -19,3 +16,23 @@ const calibrationValue = (str: string) => {
 
 // Part 1
 console.log(`Part 1: ${sum(input.map(calibrationValue))}`);
+
+// Part 2
+const replacementDict = {
+  one: "one1one",
+  two: "two2two",
+  three: "three3three",
+  four: "four4four",
+  five: "five5five",
+  six: "six6six",
+  seven: "seven7seven",
+  eight: "eight8eight",
+  nine: "nine9nine"
+};
+const preprocessedInput = input.map(line => {
+  for (const [original, replacement] of Object.entries(replacementDict)) {
+    line = line.replaceAll(original, replacement);
+  }
+  return line;
+});
+console.log(`Part 2: ${sum(preprocessedInput.map(calibrationValue))}`);
