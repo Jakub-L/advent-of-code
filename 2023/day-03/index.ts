27845eee7d1a7daa33f1b73a1b07fb88986c2577
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-12-03 21:36:14 +0000

    Solve day 3, part 2

--- 2023/day-03/index.ts
+++ 2023/day-03/index.ts
@@ -1,7 +1,7 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
-import { sum } from "@jakub-l/aoc-lib/math";
+import { prod, sum } from "@jakub-l/aoc-lib/math";
 
-type Element = { number: number; isPart: boolean };
+type Element = { number: number; isPart: boolean; adjacentGears: Set<string> };
 
 const input: string[][] = readFile(__dirname + "/input.txt", ["\n", ""]) as string[][];
 
@@ -20,13 +20,13 @@ const test = `467..114..
 
 // UTILS
 /**
- * Checks whether a number is adjacent (including diagonals) to a symbol.
+ * Checks whether a set of coordinates is adjacent (including diagonals) to a symbol.
  * @param {string[][]} schematic - The schematic to check.
  * @param {number} x - The x coordinate of the number.
  * @param {number} y - The y coordinate of the number.
  * @returns {boolean} True if any neighbouring fields are symbols (period excluded)
  */
-const checkAdjacency = (schematic: string[][], x: number, y: number): boolean => {
+const isNextToSymbol = (schematic: string[][], x: number, y: number): boolean => {
   // prettier-ignore
   const neighbourDeltas = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
   for (const [dx, dy] of neighbourDeltas) {
@@ -37,33 +37,73 @@ const checkAdjacency = (schematic: string[][], x: number, y: number): boolean =>
 };
 
 /**
+ * Returns an array of coordinates of gears neighbouring a set of coordinates.
+ * @param {string[][]} schematic - The schematic to check.
+ * @param {number} x - The x coordinate of the number.
+ * @param {number} y - The y coordinate of the number.
+ * @returns {Set<string>} A set of adjacent gear positions in the format of "x,y".
+ */
+const getNeighbouringGears = (schematic: string[][], x: number, y: number): Set<string> => {
+  const result: Set<string> = new Set();
+  // prettier-ignore
+  const neighbourDeltas = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  for (const [dx, dy] of neighbourDeltas) {
+    const neighbour = schematic[y + dy]?.[x + dx];
+    if (neighbour && neighbour === "*") result.add(`${x + dx},${y + dy}`);
+  }
+  return result;
+};
+
+/**
  * Parses a schematic and returns an array of parts.
  * @param {string[][]} schematic - The schematic to parse.
  * @returns {Element[]} The parsed parts.
  */
 const parseSchematic = (schematic: string[][]): Element[] => {
   const result: Element[] = [];
-  let part = { number: 0, isPart: false };
+  let part = { number: 0, isPart: false, adjacentGears: new Set<string>() };
   for (let y = 0; y < schematic.length; y++) {
     for (let x = 0; x < schematic[y].length; x++) {
       const char = schematic[y][x];
       if (!isNaN(Number(char))) {
         part.number = 10 * part.number + Number(char);
-        if (!part.isPart) part.isPart = checkAdjacency(schematic, x, y);
+        part.adjacentGears = new Set([...part.adjacentGears, ...getNeighbouringGears(schematic, x, y)]);
+        if (!part.isPart) part.isPart = isNextToSymbol(schematic, x, y);
       } else if (part.number > 0) {
         result.push(part);
-        part = { number: 0, isPart: false };
+        part = { number: 0, isPart: false, adjacentGears: new Set<string>() };
       }
     }
   }
   return result;
 };
 
+/**
+ * Finds all gear ratios in a schematic. A gear ratio is defined as the product of all numbers adjacent
+ * to a gear, but only if there is at least two adjacent numbers.
+ * @param {Element[]} elements - The elements to check.
+ * @returns {number[]} An array of gear ratios.
+ */
+const getGearRatios = (elements: Element[]): number[] => {
+  const partsAdjacentToGears: { [key: string]: number[] } = {};
+  for (const element of elements) {
+    if (element.adjacentGears.size > 0) {
+      for (const gear of element.adjacentGears) {
+        partsAdjacentToGears[gear] = [...(partsAdjacentToGears[gear] ?? []), element.number];
+      }
+    }
+  }
+  return Object.values(partsAdjacentToGears)
+    .filter(gears => gears.length > 1)
+    .map(prod);
+};
+
 // RESULTS
 console.log(
   `Part 1: ${sum(
-    parseSchematic(test)
+    parseSchematic(input)
       .filter(({ isPart }) => isPart)
       .map(({ number }) => number)
   )}`
 );
+console.log(`Part 2: ${sum(getGearRatios(parseSchematic(input)))}`);
