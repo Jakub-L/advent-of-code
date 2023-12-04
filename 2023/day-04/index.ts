2d534dd0130f6f7afbf7055054f63112ad5a554d
Jakub L <jakub.lech@sennen.tech>
2023-12-04 11:40:47 +0000

    Documenting and formatting

--- 2023/day-04/index.ts
+++ 2023/day-04/index.ts
@@ -10,13 +10,13 @@ type Card = {
 
 const input: string[] = readFile(__dirname + "/input.txt") as string[];
 
-const test = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`.split("\n");
-
+// UTILS
+/**
+ * Finds intersection of two sets
+ * @param {Set<T>} setA - First set
+ * @param {Set<T>} setB - Second set
+ * @returns {Set<T>} A set of elements both in setA and setB
+ */
 const intersection = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
   const result = new Set<T>();
   const [smaller, bigger] = setA.size < setB.size ? [setA, setB] : [setB, setA];
@@ -26,22 +26,27 @@ const intersection = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
   return result;
 };
 
-const calculatePoints = (matches: number): number => {
-  if (matches === 0) return 0;
-  if (matches === 1) return 1;
-  return 2 ** (matches - 1);
-};
-
-const parseCard = (card: string) => {
+/**
+ * Parses a card string into an object
+ * @param {string} card - A card string
+ * @returns {Card} The parsed Card
+ */
+const parseCard = (card: string): Card => {
   const [_, numbers] = card.split(": ");
   const [winningStrings, drawnStrings] = numbers.split(" | ").map(s => s.trim().split(/\s+/));
   const winningNumbers = new Set(winningStrings.map(Number));
   const drawnNumbers = new Set(drawnStrings.map(Number));
   const matches = intersection(winningNumbers, drawnNumbers).size;
-  const points = calculatePoints(matches);
+  const points = matches < 2 ? matches : 2 ** (matches - 1);
   return { winningNumbers, drawnNumbers, matches, points };
 };
 
+/**
+ * Processes a list of cards by adding 1 copy of N following cards, where N is the number of
+ * matches of the current card
+ * @param {Card[]} cards - A list of cards
+ * @returns {number} The total number of cards processed
+ */
 const processCards = (cards: Card[]): number => {
   const winCounts: { [index: number]: number } = {};
   for (let i = 0; i < cards.length; i++) {
@@ -52,5 +57,6 @@ const processCards = (cards: Card[]): number => {
   return sum(Object.values(winCounts)) + cards.length;
 };
 
+// RESULTS
 console.log(`Part 1: ${sum(input.map(parseCard).map(card => card.points))}`);
 console.log(`Part 2: ${processCards(input.map(parseCard))}`);
