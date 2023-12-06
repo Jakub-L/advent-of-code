66f31f140f3f19c5f78234153bc7ccbc8fcdf37e
Jakub L <jakub.lech@sennen.tech>
2023-12-06 11:51:37 +0000

    Tidy up and add documentation

--- 2023/day-06/index.ts
+++ 2023/day-06/index.ts
@@ -1,40 +1,63 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
+import { prod } from "@jakub-l/aoc-lib/math";
 
-type Race = { duration: number, record: number };
+type Race = { duration: number; record: number };
 
+const input: string[] = readFile(__dirname + "/input.txt") as string[];
 
+// INPUT PARSING
+const parseIndividualRaces = (input: string[]) => {
+  const races: Race[] = [];
+  const data: number[][] = input.map(line => line.split(/\s+/).slice(1).map(Number));
+  for (let i = 0; i < data[0].length; i++) {
+    races.push({ duration: data[0][i], record: data[1][i] });
+  }
+  return races;
+};
 
+const parseSingleRace = (input: string[]): Race => {
+  const [duration, record] = input.map(line => line.split(/\s+/).slice(1).join("")).map(Number);
+  return { duration, record };
+};
+
+// UTILS
 /**
- * distance = (raceDuration - holdTime) * holdTime
- * distance = raceDuration * holdTime - holdTime^2
- * record < distance
- * record < raceDuration * holdTime - holdTime^2
- * 0 < - holdTime^2 + raceDuration * holdTime - record
- * 
- * a = -1
- * b = raceDuration
- * c = -record
- * 
- * holdTime1 = (-raceDuration + sqrt(raceDuration^2 - 4record)) / -2
- * holdTime2 = (-raceDuration - sqrt(raceDuration^2 - 4record)) / -2
- * 
- * holdTime1 = (raceDuration - sqrt(raceDuration^2 - 4record)) / 2
- * holdTime2 = (raceDuration + sqrt(raceDuration^2 - 4record)) / 2
+ * Counts the ways to win a race.
+ *
+ * Generally, the distance travelled by the boat is:
+ *    distance = (raceDuration - holdTime) * holdTime
+ *
+ * We also want a situation where the record is beaten, so:
+ *    record < distance
+ *    record < (raceDuration - holdTime) * holdTime
+ *         0 < (raceDuration - holdTime) * holdTime - record
+ *
+ * We will need to find where this is equal to zero. This allows us to use
+ * the quadratic formula to find the two limit hold times:
+ *    (- holdTime^2) + raceDuration * holdTime - record = 0
+ *
+ *    √Δ = sqrt(raceDuration^2 - 4 * record)
+ *    holdTime1 = (-raceDuration + √Δ) / -2
+ *    holdTime2 = (-raceDuration - √Δ) / -2
+ *
+ * Knowing these, we can easily find the count of wins. There is a minor
+ * adjustment that needs to happen to account for when either of the hold
+ * times is not an integer (round up or down) or if it is an integer and
+ * equal the record exactly (add or subtract 1).
+ *
+ * @param {Race} race - The race to inspect
+ * @returns {number} The count of different hold times that will result in a win
  */
-
 const waysToWin = (race: Race): number => {
   const { duration, record } = race;
   const sqrtDelta = Math.sqrt(duration ** 2 - 4 * record);
   let lowerTime = (duration - sqrtDelta) / 2;
-  lowerTime = Math.ceil(lowerTime) === lowerTime ? lowerTime + 1 : Math.ceil(lowerTime);
   let upperTime = (duration + sqrtDelta) / 2;
+  lowerTime = Math.ceil(lowerTime) === lowerTime ? lowerTime + 1 : Math.ceil(lowerTime);
   upperTime = Math.floor(upperTime) === upperTime ? upperTime - 1 : Math.floor(upperTime);
   return upperTime - lowerTime + 1;
-}
+};
 
-console.log(waysToWin({ duration: 55, record: 401 }));
-console.log(waysToWin({ duration: 99, record: 1485 }))
-console.log(waysToWin({ duration: 97, record: 2274 }))
-console.log(waysToWin({ duration: 93, record: 1405 }))
-
-console.log(waysToWin({ duration: 55999793, record: 401148522741405 }))
+// RESULTS
+console.log(`Part 1: ${prod(parseIndividualRaces(input).map(waysToWin))}`);
+console.log(`Part 2: ${waysToWin(parseSingleRace(input))}`);
