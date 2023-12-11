ba208be10bb0a16e3be74c50c41f51c01353bcbc
Jakub L <jakub.lech@sennen.tech>
2023-12-11 16:26:29 +0000

    Tidy up solution to day 11 and add documentation

--- 2023/day-11/index.ts
+++ 2023/day-11/index.ts
@@ -1,63 +1,85 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 import { sum } from "@jakub-l/aoc-lib/math";
 
-const input = readFile(__dirname + "/input.txt", ["\n", ""]) as string[][];
-
-// const input = `...#......
-// .......#..
-// #.........
-// ..........
-// ......#...
-// .#........
-// .........#
-// ..........
-// .......#..
-// #...#.....`
-//   .split("\n")
-//   .map(line => line.split(""));
-
+// UTILS
+/** Class representing the map of the sky */
 class SkyMap {
-  galaxies: number[][] = [];
-  emptyRows: boolean[];
-  emptyCols: boolean[];
-  _spaceExpanded: boolean = false;
+  /** Array of [row, col] positions of the galaxies in the original image */
+  private _imageGalaxyPositions: number[][] = [];
+  /** Array of [row, col] positions of the galaxies accounting for space expansion */
+  private _actualGalaxyPositions: number[][] = [];
+  /** Array of booleans representing if a particular row is empty */
+  private _isRowEmpty: boolean[];
+  /** Array of booleans representing if a particular column is empty */
+  private _isColumnEmpty: boolean[];
 
-  constructor(image: string[][], private _expansionFactor: number = 2) {
-    this.emptyRows = Array.from({ length: image.length }).fill(true) as boolean[];
-    this.emptyCols = Array.from({ length: image[0].length }).fill(true) as boolean[];
+  /**
+   * Creates a new SkyMap
+   * @param {string[][]} image - An image of the sky, with hashes (#) representing a galaxy
+   *    and full stops (.) representing empty space
+   */
+  constructor(image: string[][]) {
+    this._isRowEmpty = Array.from({ length: image.length }).fill(true) as boolean[];
+    this._isColumnEmpty = Array.from({ length: image[0].length }).fill(true) as boolean[];
     for (let row = 0; row < image.length; row++) {
       for (let col = 0; col < image[row].length; col++) {
         if (image[row][col] === "#") {
-          this.emptyRows[row] = false;
-          this.emptyCols[col] = false;
-          this.galaxies.push([row, col]);
+          this._isRowEmpty[row] = false;
+          this._isColumnEmpty[col] = false;
+          this._imageGalaxyPositions.push([row, col]);
         }
       }
     }
   }
 
-  _generateCumulativeShift(emptyLines: boolean[]) {
-    return emptyLines.reduce((acc, isEmpty) => {
+  /**
+   * Expands empty space by a given factor. Adjusts the actual positions of all
+   * galaxies on the map to represent the space expansion.
+   *
+   * @param {number} [expansionFactor=2] - How much each empty space should occupy.
+   *      Defaults to 2, meaning that each empty row or column is treated as 2.
+   */
+  expandSpace(expansionFactor: number = 2): SkyMap {
+    const rowShift = this._getCumulativeShift(this._isRowEmpty, expansionFactor);
+    const colShift = this._getCumulativeShift(this._isColumnEmpty, expansionFactor);
+    this._actualGalaxyPositions = this._imageGalaxyPositions.map(([row, col]) => [
+      row + rowShift[row],
+      col + colShift[col]
+    ]);
+    return this;
+  }
+
+  /**
+   * Converts an array of booleans representing if a given position is empty to an array of
+   * cumulative shifts for each position.
+   *
+   * For example, if the output is [0, 0, 1, 1], it means that anything in the first two
+   * positions should not be shifted, and anything in the last two positions should be shifted
+   * by 1.
+   *
+   * @param {boolean[]} posShouldExpand - Array of booleans representing if a given position
+   *     needs to be expanded (i.e. is empty)
+   * @param {number} expansionFactor - Factor by which to expand the space
+   * @returns {number[]} Array of cumulative shifts for each position
+   */
+  private _getCumulativeShift(posShouldExpand: boolean[], expansionFactor: number) {
+    return posShouldExpand.reduce((acc, isEmpty) => {
       const prev = acc.at(-1) ?? 0;
-      acc.push(prev + (isEmpty ? (this._expansionFactor - 1) : 0));
+      acc.push(prev + (isEmpty ? expansionFactor - 1 : 0));
       return acc;
     }, [] as number[]);
   }
 
-  _expandSpace() {
-    const cumulativeRowShift = this._generateCumulativeShift(this.emptyRows);
-    const cumulativeColShift = this._generateCumulativeShift(this.emptyCols);
-    this.galaxies = this.galaxies.map(([row, col]) => [row + cumulativeRowShift[row], col + cumulativeColShift[col]]);
-    this._spaceExpanded = true;
-  }
-
-  _getShortestPathSum() {
-    if (!this._spaceExpanded) this._expandSpace();
+  /**
+   * Finds the shortest paths between all galaxy pairs and returns their sum.
+   * Only counts each pair once, i.e. A to B is the same as B to A.
+   */
+  get shortestPathSum() {
     const shortestPaths = [];
-    for (let i = 0; i < this.galaxies.length; i++) {
-      const [sourceRow, sourceCol] = this.galaxies[i];
-      for (let j = i + 1; j < this.galaxies.length; j++) {
-        const [targetRow, targetCol] = this.galaxies[j];
+    for (let i = 0; i < this._actualGalaxyPositions.length; i++) {
+      const [sourceRow, sourceCol] = this._actualGalaxyPositions[i];
+      for (let j = i + 1; j < this._actualGalaxyPositions.length; j++) {
+        const [targetRow, targetCol] = this._actualGalaxyPositions[j];
         const path = Math.abs(targetRow - sourceRow) + Math.abs(targetCol - sourceCol);
         shortestPaths.push(path);
       }
@@ -66,5 +88,10 @@ class SkyMap {
   }
 }
 
-const map = new SkyMap(input, 1_000_000);
-console.log(map._getShortestPathSum());
+// INPUT PROCESSING
+const input = readFile(__dirname + "/input.txt", ["\n", ""]) as string[][];
+const map = new SkyMap(input);
+
+// RESULTS
+console.log(`Part 1: ${map.expandSpace().shortestPathSum}`);
+console.log(`Part 2: ${map.expandSpace(1_000_000).shortestPathSum}`);
