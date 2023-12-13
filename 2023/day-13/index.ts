0826f100b5b6255cb8949c562e06234a9225e484
Jakub L <jakub.lech@sennen.tech>
2023-12-13 16:26:50 +0000

    Solve day 13, part 2

--- 2023/day-13/index.ts
+++ 2023/day-13/index.ts
@@ -26,19 +26,28 @@ const input = readFile(__dirname + "/input.txt", ["\n\n", "\n", ""]) as string[]
 class Pattern {
   private _rowMap: string[];
   private _colMap: string[] = [];
-  private _rowsAboveReflection: number = -1;
-  private _columnsLeftOfReflection: number = -1;
 
   constructor(mirrorMap: string[][]) {
     this._rowMap = mirrorMap.map(row => row.join(""));
     this._colMap = mirrorMap[0].map((_, col) => mirrorMap.map(row => row[col]).join(""));
-    this._columnsLeftOfReflection = this._findMirrorLine(this._colMap);
-    if (this._columnsLeftOfReflection <= 0) this._rowsAboveReflection = this._findMirrorLine(this._rowMap);
+  }
+
+  summary(lookForSmudge: boolean = false): number {
+    const colLine = lookForSmudge ? this._findSmudgeLine(this._colMap) : this._findMirrorLine(this._colMap);
+    const rowLine = lookForSmudge ? this._findSmudgeLine(this._rowMap) : this._findMirrorLine(this._rowMap);
+    return colLine > 0 ? colLine : 100 * rowLine;
   }
 
   private _findMirrorLine(map: string[]): number {
     for (let i = 0; i < map.length - 1; i++) {
       if (this._isMirrorLine(map, i)) return i + 1;
+    }
+    return 0;
+  }
+
+  private _findSmudgeLine(map: string[]): number {
+    for (let i = 0; i < map.length - 1; i++) {
+      if (this._hasOneSmudge(map, i)) return i + 1;
     }
     return 0;
   }
@@ -50,11 +59,20 @@ class Pattern {
     return true;
   }
 
-  get summary(): number {
-    return this._columnsLeftOfReflection > 0 ? this._columnsLeftOfReflection : 100 * this._rowsAboveReflection;
+  private _hasOneSmudge(map: string[], line: number): boolean {
+    let smudgeFound = false;
+    for (let i = line, j = line + 1; i >= 0 && j < map.length; i--, j++) {
+      for (let k = 0; k < map[i].length; k++) {
+        if (map[i][k] !== map[j][k]) {
+          // If we already had a smudge, this is a second smudge, which is not allowed
+          if (smudgeFound) return false;
+          smudgeFound = true;
+        }
+      }
+    }
+    return smudgeFound;
   }
 }
 
-console.log(sum(input.map(mirrorMap => new Pattern(mirrorMap).summary)));
-
-// console.log(new Pattern(input[0]).summary);
+// console.log(sum(input.map(mirrorMap => new Pattern(mirrorMap).summary())));
+console.log(sum(input.map(mirrorMap => new Pattern(mirrorMap).summary(true))));
