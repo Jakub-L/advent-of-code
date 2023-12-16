154812216400101f2fd3d4b885df36f2e3d2b9db
Jakub L <jakub.lech@sennen.tech>
2023-12-16 23:03:53 +0100

    Refactor code, add documentation

--- 2023/day-16/index.ts
+++ 2023/day-16/index.ts
@@ -1,18 +1,7 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 
-// const input = `.|...\\....
-// |.-.\\.....
-// .....|-...
-// ........|.
-// ..........
-// .........\\
-// ..../.\\\\..
-// .-.-/..|..
-// .|....-|.\\
-// ..//.|....`
-//   .split("\n")
-//   .map(line => line.split(""));
-
+// UTILS
+type Tile = "/" | "\\" | "|" | "-" | ".";
 enum Direction {
   Right,
   Left,
@@ -20,71 +9,123 @@ enum Direction {
   Down
 }
 
-class Beam {
-  direction: Direction;
-  private _row: number;
-  private _col: number;
+/** The mapping of direction of movement to row and column deltas */
+const directionToDelta: Record<Direction, number[]> = {
+  [Direction.Up]: [-1, 0],
+  [Direction.Right]: [0, 1],
+  [Direction.Down]: [1, 0],
+  [Direction.Left]: [0, -1]
+};
 
+/** The mapping of incoming direction and encountered character to output directions */
+const charToOutputDirections: Record<Tile, Record<Direction, Direction[]>> = {
+  "/": {
+    [Direction.Up]: [Direction.Right],
+    [Direction.Right]: [Direction.Up],
+    [Direction.Down]: [Direction.Left],
+    [Direction.Left]: [Direction.Down]
+  },
+  "\\": {
+    [Direction.Up]: [Direction.Left],
+    [Direction.Right]: [Direction.Down],
+    [Direction.Down]: [Direction.Right],
+    [Direction.Left]: [Direction.Up]
+  },
+  "|": {
+    [Direction.Up]: [Direction.Up],
+    [Direction.Down]: [Direction.Down],
+    [Direction.Right]: [Direction.Up, Direction.Down],
+    [Direction.Left]: [Direction.Up, Direction.Down]
+  },
+  "-": {
+    [Direction.Up]: [Direction.Right, Direction.Left],
+    [Direction.Down]: [Direction.Right, Direction.Left],
+    [Direction.Right]: [Direction.Right],
+    [Direction.Left]: [Direction.Left]
+  },
+  ".": {
+    [Direction.Up]: [Direction.Up],
+    [Direction.Down]: [Direction.Down],
+    [Direction.Right]: [Direction.Right],
+    [Direction.Left]: [Direction.Left]
+  }
+};
+
+/** A single beam moving through the contraption */
+class Beam {
+  /** The direction the beam is moving */
+  direction: Direction;
+  /** The row of the beam front */
+  row: number;
+  /** The column of the beam front */
+  col: number;
+
+  /**
+   * Create a new beam
+   * @param {object} options - The options for the beam
+   * @param {number} options.row - The row of the beam front. Defaults to 0
+   * @param {number} options.col - The column of the beam front. Defaults to 0
+   * @param {Direction} options.direction - The direction the beam is moving. Defaults to Right
+   */
   constructor(options: { row?: number; col?: number; direction?: Direction } = {}) {
     const { row = 0, col = 0, direction = Direction.Right } = options;
-    this._row = row;
-    this._col = col;
+    this.row = row;
+    this.col = col;
     this.direction = direction;
   }
 
+  /**
+   * Move the beam one step forward along its current direction.
+   * @returns {Beam} The beam after moving one step
+   */
   step(): Beam {
-    const [dR, dC] = this._getDeltas(this.direction);
-    this._row += dR;
-    this._col += dC;
+    const [dR, dC] = directionToDelta[this.direction];
+    this.row += dR;
+    this.col += dC;
     return this;
   }
 
-  private _getDeltas(direction: Direction): [dR: number, dC: number] {
-    switch (direction) {
-      case Direction.Up:
-        return [-1, 0];
-      case Direction.Right:
-        return [0, 1];
-      case Direction.Down:
-        return [1, 0];
-      case Direction.Left:
-        return [0, -1];
-    }
-  }
-
-  get row(): number {
-    return this._row;
-  }
-
-  get col(): number {
-    return this._col;
-  }
-
+  /**
+   * Get a unique identifier for the beam, based on its position and direction
+   * @returns {string} The identifier of the beam
+   */
   get id(): string {
     return `${this.pos},${this.direction}`;
   }
 
+  /**
+   * Get the current position of the beam
+   * @returns {string} The position of the beam
+   */
   get pos(): string {
-    return `${this._row},${this._col}`;
+    return `${this.row},${this.col}`;
   }
 }
 
 class Contraption {
-  private _layout: string[][] = [];
+  /** The contraption layout */
+  private _layout: Tile[][] = [];
+  /** The width of the layout (number of columns) */
   private _width: number;
+  /** The height of the layout (number of rows) */
   private _height: number;
 
-  constructor(layout: string[][]) {
+  /**
+   * Create a new contraption
+   * @param {Tile[][]} layout - The layout of the contraption
+   */
+  constructor(layout: Tile[][]) {
     this._layout = layout;
     this._width = layout[0].length;
     this._height = layout.length;
   }
 
-  maxEdgeEnergizing(): number {
-    const bounces = this._getEdgeStarts().map(start => this.bounce(start));
-    return Math.max(...bounces);
-  }
-
+  /**
+   * Simulate a beam bouncing around the contraption.
+   * @param {Beam} startBeam - The beam to start with. Defaults to a beam starting at the top left,
+   *       moving right.
+   * @returns {number} The number of tiles that will be energized.
+   */
   bounce(startBeam: Beam): number {
     const seenBeams: Set<string> = new Set([startBeam.id]);
     const energized: Set<string> = new Set([startBeam.pos]);
@@ -104,17 +145,44 @@ class Contraption {
     return energized.size;
   }
 
+  /**
+   * Finds the maximum possible number of energized tiles, assuming the beams can enter
+   * from any edge tile.
+   * @returns {number} The maximum number of tiles that will be energized.
+   */
+  maxEdgeEnergizing(): number {
+    const bounces = this._getEdgeStarts().map(start => this.bounce(start));
+    return Math.max(...bounces);
+  }
+
+  /**
+   * Get the beams that would be output from a given beam's current position. This takes
+   * into account the Tile at the beam's current position and the direction it is moving.
+   * @param {Beam} beam - The beam to get the output beams from
+   * @returns {Beam[]} The beams that would be output from the incoming beam hitting the current
+   *        character.
+   */
   private _getOutputBeams(beam: Beam): Beam[] {
     const { row, col, direction } = beam;
-    let outputDirections: Direction[] = this._getOutDirections(this._layout[row][col], direction);
+    let outputDirections: Direction[] = charToOutputDirections[this._layout[row][col]][direction];
     return outputDirections.map(newDirection => new Beam({ row, col, direction: newDirection }));
   }
 
+  /**
+   * Checks if a beam is still within bounds of a layout
+   * @param {Beam} beam - Beam to check
+   * @returns {boolean} True if beam's current position is within the layout
+   */
   private _isInBounds(beam: Beam): boolean {
     const { row, col } = beam;
     return row >= 0 && row < this._height && col >= 0 && col < this._width;
   }
 
+  /**
+   * Get the beams starting on the edges of the contraption. For corner locations,
+   * two beams will be returned, one for each possible direction.
+   * @returns {Beam[]} The beams starting on the edges
+   */
   private _getEdgeStarts(): Beam[] {
     const starts: Beam[] = [];
     for (let col = 0; col < this._width; col++) {
@@ -131,49 +199,11 @@ class Contraption {
     }
     return starts;
   }
-
-  private _getOutDirections(char: string, incomingDirection: Direction): Direction[] {
-    switch (char) {
-      case "/":
-        return {
-          [Direction.Up]: [Direction.Right],
-          [Direction.Right]: [Direction.Up],
-          [Direction.Down]: [Direction.Left],
-          [Direction.Left]: [Direction.Down]
-        }[incomingDirection];
-      case "\\":
-        return {
-          [Direction.Up]: [Direction.Left],
-          [Direction.Right]: [Direction.Down],
-          [Direction.Down]: [Direction.Right],
-          [Direction.Left]: [Direction.Up]
-        }[incomingDirection];
-      case "|":
-        return {
-          [Direction.Up]: [Direction.Up],
-          [Direction.Down]: [Direction.Down],
-          [Direction.Right]: [Direction.Up, Direction.Down],
-          [Direction.Left]: [Direction.Up, Direction.Down]
-        }[incomingDirection];
-      case "-":
-        return {
-          [Direction.Up]: [Direction.Right, Direction.Left],
-          [Direction.Down]: [Direction.Right, Direction.Left],
-          [Direction.Right]: [Direction.Right],
-          [Direction.Left]: [Direction.Left]
-        }[incomingDirection];
-      default:
-        return [incomingDirection];
-    }
-  }
 }
 
 // INPUT PROCESSING
-const input = readFile(__dirname + "/input.txt", ["\n", ""]) as string[][];
+const input = readFile(__dirname + "/input.txt", ["\n", ""]) as Tile[][];
 
-console.time("part1");
-console.log(new Contraption(input).bounce(new Beam()));
-console.timeEnd("part1");
-console.time("part2");
-console.log(new Contraption(input).maxEdgeEnergizing());
-console.timeEnd("part2");
+// RESULTS
+console.log(`Part 1: ${new Contraption(input).bounce(new Beam())}`);
+console.log(`Part 2: ${new Contraption(input).maxEdgeEnergizing()}`);
