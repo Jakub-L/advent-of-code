0ff3aa0f6a5edc1c68fac89b7a2d9d32da59bd75
Jakub L <jakub.lech@sennen.tech>
2023-12-18 16:09:46 +0100

    Return correct answer for test data

--- 2023/day-17/index.ts
+++ 2023/day-17/index.ts
@@ -16,4 +16,72 @@ import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 //   .split("\n")
 //   .map(line => line.split("").map(Number));
 
-// const input = readFile(__dirname + "/input.txt", ["\n", ""], x => Number(x)) as unknown[][] as number[][];
+const input = readFile(__dirname + "/input.txt", ["\n", ""], x => Number(x)) as unknown[][] as number[][];
+
+/**
+ * Enumeration of directions: Up, Right, Down, Left. This arrangement means that
+ * for i-th direction, [(i + 2) % 4]-th direction is the reverse direction
+ */
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left
+}
+
+const directionArray: Direction[] = [Direction.Up, Direction.Right, Direction.Down, Direction.Left];
+const directionDelta: Record<Direction, [number, number]> = {
+  [Direction.Up]: [-1, 0],
+  [Direction.Right]: [0, 1],
+  [Direction.Down]: [1, 0],
+  [Direction.Left]: [0, -1]
+};
+
+interface DijkstraOptions {
+  start?: [number, number];
+  end?: [number, number];
+}
+
+export const dijkstra = (grid: number[][], options: DijkstraOptions = {}): number => {
+  const { start = [0, 0], end = [grid.length - 1, grid[0].length - 1] } = options;
+  const visited = new Set();
+  const [y0, x0] = start;
+  const [yt, xt] = end;
+  const queue: { x: number; y: number; heat: number; history: Direction[] }[] = [
+    { x: x0, y: y0, heat: 0, history: [] }
+  ];
+
+  while (queue.length) {
+    const { x, y, heat, history } = queue.shift()!;
+    // If we made it to the end, return distance to it
+    if (x === xt && y === yt) return heat;
+    // Otherwise, check every neighbour
+    const neighbours: Direction[] = getNeighbours(history.slice(-3));
+    for (let direction of neighbours) {
+      const [dx, dy] = directionDelta[direction];
+      const [xx, yy] = [x + dx, y + dy];
+      const id = `${xx},${yy},${direction},[${history.slice(-3).join(",")}]`;
+      // If point is outside grid bounds or has been visited, skip it
+      if (grid[yy]?.[xx] === undefined || visited.has(id)) continue;
+      // Otherwise mark the point as visited and add it to the queue of vertices to check
+      visited.add(id);
+      queue.push({ x: xx, y: yy, heat: heat + grid[yy][xx], history: [...history, direction] });
+    }
+    // Sort the queue to find the shortest-distance vertex to inspect next
+    queue.sort((a, b) => a.heat - b.heat);
+  }
+  return -1;
+};
+
+const getNeighbours = (directionHistory: Direction[]): Direction[] => {
+  // Can't go back
+  let allowedDirections = directionArray.filter(dir => (dir + 2) % 4 !== dir);
+  if (directionHistory.length >= 3 && directionHistory.every(dir => dir === directionHistory[0])) {
+    allowedDirections = allowedDirections.filter(dir => dir !== directionHistory[0]);
+  }
+  return allowedDirections;
+};
+
+console.log(dijkstra(input));
+console.log("Part 1 should be: 1256");
+console.log("Part 2 should be: 1382");
