46a925e86b75e950a9dfbfd13c9070a0c7f5c264
Jakub L <jakub.lech@sennen.tech>
2023-12-29 23:45:43 +0100

    Solve day 22, part 2

--- 2023/day-22/index.ts
+++ 2023/day-22/index.ts
@@ -1,4 +1,6 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
+import { Queue } from "@jakub-l/aoc-lib/data-structures";
+import { sum } from "@jakub-l/aoc-lib/math";
 
 // UTILS
 type NumRange = { min: number; max: number };
@@ -27,8 +29,10 @@ class Tower {
   public bricks: Brick[] = [];
   /** Map of x, y coodrinates (as string) to highest Z value and brick that occupies it */
   _towerTop: Map<string, [number, number]> = new Map();
-  /** Map of brick index to array of brick indices that support it */
-  _supportedBy: Map<number, number[]> = new Map();
+  /** Array of which bricks support a given brick (i.e. _supportedBy[3] is the list of indeces that support bricks[3]) */
+  _supportedBy: Set<number>[] = [];
+  /** Array of which bricks are supported by a given brick (i.e. _supports[3] is the list of indeces that are supported by bricks[3]) */
+  _supports: Set<number>[] = [];
 
   constructor(bricks: Brick[]) {
     this.bricks = bricks.sort((a, b) => a.z.min - b.z.min);
@@ -61,22 +65,55 @@ class Tower {
           this._towerTop.set(`${x},${y}`, [brick.z.max, i]);
         }
       }
+      supportedBy.delete(NaN);
+      this._supportedBy.push(supportedBy);
 
-      this._supportedBy.set(i, Array.from(supportedBy));
+      for (const supportingBrick of supportedBy) {
+        if (!this._supports[supportingBrick]) this._supports[supportingBrick] = new Set();
+        this._supports[supportingBrick].add(i);
+      }
     }
+  }
+
+  getChainReaction(start: number) {
+    const supportedByCount = this._supportedBy.map(supports => supports.size);
+    // Even though we technically disintegrate the first brick, this is kind of
+    // like a fall, in terms of behaviour. We have to set the fall count to -1
+    // or else it would _actually_ count as a fall
+    let fallCount = -1;
+    const queue = new Queue<number>();
+    queue.enqueue(start);
+    while (!queue.isEmpty) {
+      fallCount++;
+      const brick = queue.dequeue();
+      // For each Brick that is supported by this brick, decrement their support count
+      // If the support count reaches 0, that means that the brick will fall.
+      for (const supportedBrick of this._getSupportedBricks(brick)) {
+        supportedByCount[supportedBrick]--;
+        if (supportedByCount[supportedBrick] === 0) queue.enqueue(supportedBrick);
+      }
+    }
+    return fallCount;
   }
 
   private _getTop(x: number, y: number): [number, number] {
     return this._towerTop.get(`${x},${y}`) || [0, NaN];
   }
 
+  private _getSupportedBricks(i: number): Set<number> {
+    return this._supports[i] ?? new Set<number>();
+  }
+
   get removableBrickCount(): number {
     const unremovableBricks: Set<number> = new Set();
-    for (let supports of this._supportedBy.values()) {
-      if (supports.length === 1) unremovableBricks.add(supports[0]);
+    for (let supports of this._supportedBy) {
+      if (supports.size === 1) unremovableBricks.add(supports.values().next().value);
     }
-    unremovableBricks.delete(NaN);
     return this.bricks.length - unremovableBricks.size;
+  }
+
+  get chainReactionSum(): number {
+    return sum(this._supportedBy.map((_, i) => this.getChainReaction(i)));
   }
 }
 
@@ -95,5 +132,7 @@ tower.drop();
 
 // RESULTS
 console.log(tower.removableBrickCount);
+console.log(tower.chainReactionSum);
+
 // console.log(tower._supportedBy);
 // console.log(tower.bricks.map(brick => brick.toString()).join("\n"));
