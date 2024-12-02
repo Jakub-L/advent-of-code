67333c1f54ca71577e7933226da2c88771a2c847
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2024-12-02 13:38:40 +0000

    Tidy up day 2 solutions

--- 2024/day-02/index.ts
+++ 2024/day-02/index.ts
@@ -1,28 +1,54 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
-import { report } from "process";
 
-const reports: number[][] = readFile(__dirname + "/input.txt", ["\n", " "], Number) as number[][];
+const MIN_CHANGE = 1;
+const MAX_CHANGE = 3;
 
-const isSafe = (report: number[]): boolean => {
-  let prevSign = Math.sign(report[1] - report[0]);
+type Level = number;
+type Report = Level[];
+
+const reports: Report[] = readFile(__dirname + "/input.txt", ["\n", " "], Number) as Report[];
+
+// Part 1
+/**
+ * Finds whether a report is safe.
+ *
+ * A report is safe if:
+ * - The levels are either all increasing or all decreasing.
+ * - Any two adjacent levels differ by at least one and at most three.
+ *
+ * @param {Report} report - The report to check.
+ * @returns {boolean} - Whether the report is safe.
+ */
+const isSafe = (report: Report): boolean => {
+  let firstChange = Math.sign(report[1] - report[0]);
   for (let i = 0; i < report.length - 1; i++) {
     let diff = report[i + 1] - report[i];
-    if (Math.abs(diff) > 3 || Math.abs(diff) < 1) return false;
-    if (prevSign !== Math.sign(diff)) return false;
+    if (Math.abs(diff) > MAX_CHANGE || Math.abs(diff) < MIN_CHANGE) return false;
+    if (firstChange !== Math.sign(diff)) return false;
   }
   return true;
 };
 
-const isSafeWithTolerance = (report: number[]): boolean => {
+// Part 2
+/**
+ * Checks whether a report is safe with a tolerance of one level. This means
+ * a report is marked as safe if it is safe as-is, or if it would be safe
+ * if any one level was removed.
+ *
+ * @param {Report} report - The report to check.
+ * @returns {boolean} - Whether the report is safe.
+ */
+const isSafeWithTolerance = (report: Report): boolean => {
   const isBaseSafe = isSafe(report);
   if (isBaseSafe) return true;
 
   for (let i = 0; i < report.length; i++) {
-    const newReport = [...report.slice(0, i), ...report.slice(i + 1)];
+    const newReport = report.toSpliced(i, 1);
     if (isSafe(newReport)) return true;
   }
   return false;
 };
 
-console.log(reports.filter(isSafe).length);
-console.log(reports.filter(isSafeWithTolerance).length);
+// Results
+console.log(`Part 1: ${reports.filter(isSafe).length}`);
+console.log(`Part 2: ${reports.filter(isSafeWithTolerance).length}`);
