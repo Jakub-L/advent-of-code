060e16b598a700ed92f8e0a5e5fc1af8b7da8951
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2024-12-05 22:54:37 +0000

    Solve day 5

--- 2024/day-05/index.ts
+++ 2024/day-05/index.ts
@@ -2,44 +2,22 @@ import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 import { intersection } from "@jakub-l/aoc-lib/collections";
 import { sum } from "@jakub-l/aoc-lib/math";
 
-const sample = `47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+type PageLookup = Record<number, Set<number>>;
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47`;
-
-type RuleLookup = Record<number, Set<number>>;
-
+// Inputs
 const [rawRules, rawUpdates]: [string, string] = readFile(__dirname + "/input.txt", ["\n\n"]);
-
-const rules: RuleLookup = parseRules(rawRules);
+const rules: PageLookup = parseRules(rawRules);
 const updates: number[][] = rawUpdates.split("\n").map(update => update.split(",").map(Number));
 
-function parseRules(rawRules: string): RuleLookup {
-  const rules: RuleLookup = {};
+/**
+ * Parse the rules into a lookup table. The rules are keyed by a page and its
+ * value is a set of pages that must be after the key page.
+ *
+ * @param {string} rawRules - Raw rules
+ * @returns {PageLookup} Parsed rules
+ */
+function parseRules(rawRules: string): PageLookup {
+  const rules: PageLookup = {};
   for (const rule of rawRules.split("\n")) {
     const [firstPage, secondPage] = rule.split("|").map(Number);
     if (!rules[firstPage]) rules[firstPage] = new Set();
@@ -48,7 +26,15 @@ function parseRules(rawRules: string): RuleLookup {
   return rules;
 }
 
-function isUpdateValid(update: number[], rules: RuleLookup): boolean {
+// Part 1
+/**
+ * Check if an update is valid.
+ *
+ * @param {number[]} update - Updated page numbers
+ * @param {PageLookup} rules - Ordering rules that must be followed
+ * @returns {boolean} True if the update is valid
+ */
+function isUpdateValid(update: number[], rules: PageLookup): boolean {
   const seen: Set<number> = new Set();
   for (const page of update) {
     const pageRules = rules[page] ?? new Set();
@@ -58,9 +44,38 @@ function isUpdateValid(update: number[], rules: RuleLookup): boolean {
   return true;
 }
 
+/**
+ * Get the middle number of an array.
+ *
+ * @param {number[]} arr - Array of numbers
+ * @returns {number} Middle number
+ */
 function getMiddleNumber(arr: number[]): number {
   return arr[Math.floor(arr.length / 2)];
 }
 
-console.log(sum(updates.filter(update => isUpdateValid(update, rules)).map(getMiddleNumber)));
-// console.log(sortUpdate([75, 97, 47, 61, 53], rules));
+// Part 2
+/**
+ * Sorts an update such that it follows the page ordering rules.
+ *
+ * @param {number[]} update - Updated page numbers
+ * @param {PageLookup} rules - Ordering rules that must be followed
+ * @returns {number[]} Copy of the update array sorted according to the rules
+ */
+function sortUpdate(update: number[], rules: PageLookup): number[] {
+  const updateSet = new Set(update);
+  const pagesAfter: PageLookup = {};
+  for (const page of update) {
+    pagesAfter[page] = intersection(rules[page] ?? new Set(), updateSet);
+  }
+  return update.toSorted((a, b) => pagesAfter[b].size - pagesAfter[a].size);
+}
+
+// Results
+const validUpdates = updates.filter(update => isUpdateValid(update, rules));
+const fixedInvalidUpdates = updates
+  .filter(update => !isUpdateValid(update, rules))
+  .map(update => sortUpdate(update, rules));
+
+console.log(`Part 1: ${sum(validUpdates.map(getMiddleNumber))}`);
+console.log(`Part 2: ${sum(fixedInvalidUpdates.map(getMiddleNumber))}`);
