4748c17f14a6d28986d2b217e2e7e74fa929aad8
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2024-12-06 23:06:48 +0000

    Solve day 6

--- 2024/day-06/index.ts
+++ 2024/day-06/index.ts
@@ -1,5 +1,13 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 
+type Coord = { x: number; y: number };
+enum Dir {
+  UP,
+  RIGHT,
+  DOWN,
+  LEFT
+}
+
 const sample = `....#.....
 .........#
 ..........
@@ -16,74 +24,100 @@ const sample = `....#.....
 const input: string[][] = readFile(__dirname + "/input.txt", ["\n", ""]) as string[][];
 
 // Utils
-const dirAfterTurn: Record<string, [number, number]> = {
-  "-1,0": [0, 1],
-  "0,1": [1, 0],
-  "1,0": [0, -1],
-  "0,-1": [-1, 0]
+const id = (c: Coord) => `${c.y},${c.x}`;
+
+const coord: { [key in Dir]: Coord } = {
+  [Dir.UP]: { y: -1, x: 0 },
+  [Dir.RIGHT]: { y: 0, x: 1 },
+  [Dir.DOWN]: { y: 1, x: 0 },
+  [Dir.LEFT]: { y: 0, x: -1 }
 };
 
-class Map {
+const turn: { [key in Dir]: Dir } = {
+  [Dir.UP]: Dir.RIGHT,
+  [Dir.RIGHT]: Dir.DOWN,
+  [Dir.DOWN]: Dir.LEFT,
+  [Dir.LEFT]: Dir.UP
+};
+
+class Lab {
   private _width: number;
   private _height: number;
-  private _guard: [number, number] = [-1, -1];
-  private _direction: [number, number] = [-1, 0];
-  private _obstructions: Set<string> = new Set();
-  private _loopObstructions: Set<string> = new Set();
-  visited: Set<string> = new Set();
+  private _guard: Coord = { y: -1, x: -1 };
+  private _direction: Dir = Dir.UP;
+  public starting: Coord = { y: -1, x: -1 };
+  public obstructions: Set<string> = new Set();
+  public visited: Map<string, Set<Dir>> = new Map();
+  public loops: boolean = false;
 
   constructor(map: string[][]) {
     this._height = map.length;
     this._width = map[0].length;
-    for (let row = 0; row < map.length; row++) {
-      for (let col = 0; col < map[row].length; col++) {
-        const char = map[row][col];
-        if (char === "#") this._obstructions.add(`${row},${col}`);
-        if (char === "^") this._guard = [row, col];
+    for (let y = 0; y < map.length; y++) {
+      for (let x = 0; x < map[y].length; x++) {
+        const char = map[y][x];
+        if (char === "#") this.obstructions.add(`${y},${x}`);
+        if (char === "^") {
+          this._guard = { y, x };
+          this.starting = { y, x };
+        }
       }
     }
   }
 
   private _isInBounds(): boolean {
-    return (
-      this._guard[0] >= 0 &&
-      this._guard[0] < this._height &&
-      this._guard[1] >= 0 &&
-      this._guard[1] < this._width
-    );
-  }
-
-  private _turnRight() {
-    this._direction = dirAfterTurn[this._direction.join(",")];
+    const { x, y } = this._guard;
+    return y >= 0 && y < this._height && x >= 0 && x < this._width;
   }
 
   private _step() {
-    const [r, c] = this._guard;
-    const [dr, dc] = this._direction;
-    const newPos: [number, number] = [r + dr, c + dc];
-    if (this._obstructions.has(newPos.join(","))) this._turnRight();
+    const { y, x } = this._guard;
+    const { y: dy, x: dx } = coord[this._direction];
+    const newPos: Coord = { y: y + dy, x: x + dx };
+    if (this.obstructions.has(id(newPos))) this._direction = turn[this._direction];
     else this._guard = newPos;
   }
 
-  private _checkLoopObstruction() {
-    const [r, c] = this._guard;
-    const [dr, dc] = dirAfterTurn[this._direction.join(",")];
-    const potentialObstruction: string = `${r + dr},${c + dc}`;
-    if (this.visited.has(this._guard.join(",")) && this.visited.has(potentialObstruction)) {
-      this._loopObstructions.add(potentialObstruction);
-    }
+  private _loops(): boolean {
+    const location = id(this._guard);
+    const locationDirs = this.visited.get(location) ?? new Set();
+    return locationDirs.has(this._direction);
+  }
+
+  get potentialLoopObstacles(): string[] {
+    return [...this.visited.keys()].filter(loc => loc !== id(this.starting));
   }
 
   patrol() {
+    this._guard = this.starting;
     while (this._isInBounds()) {
-      this.visited.add(this._guard.join(","));
-      this._checkLoopObstruction();
+      if (this._loops()) {
+        this.loops = true;
+        break;
+      }
+      const location = id(this._guard);
+      const locationDirs = this.visited.get(location) ?? new Set();
+      locationDirs.add(this._direction);
+      this.visited.set(location, locationDirs);
       this._step();
     }
-    console.log(this._loopObstructions);
   }
 }
 
-const map = new Map(sample);
-map.patrol();
-console.log(map.visited.size);
+// Part 2
+const countLoops = (map: string[][], potential: string[]): number => {
+  let loopCount = 0;
+  for (const location of potential) {
+    const newLab = new Lab(map);
+    newLab.obstructions.add(location);
+    newLab.patrol();
+    if (newLab.loops) loopCount++;
+  }
+
+  return loopCount;
+};
+
+const lab = new Lab(input);
+lab.patrol();
+console.log(lab.visited.size);
+console.log(countLoops(input, lab.potentialLoopObstacles));
