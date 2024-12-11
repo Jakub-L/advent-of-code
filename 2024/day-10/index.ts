b587c78a6a307e31bc9909039ee76e3fb5b4e1e2
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2024-12-11 22:06:30 +0000

    Solve day 10

--- 2024/day-10/index.ts
+++ 2024/day-10/index.ts
@@ -15,6 +15,14 @@ type MapLocation = {
   height: number;
 };
 
+type Node = {
+  x: number;
+  y: number;
+  height: number;
+  parent: Node | null;
+  children: Node[];
+};
+
 const sample = `89010123
 78121874
 87430965
@@ -28,52 +36,77 @@ const sample = `89010123
 
 const input: number[][] = readFile(`${__dirname}/input.txt`, ["\n", ""], Number) as number[][];
 
-// Part 1
+// Part 1 & 2
+class Trail {
+  public _head: Node;
+  public _ends: Set<string> = new Set<string>();
+  public _rating: number = 0;
 
-class TopographicMap {
-  private _map: number[][];
-  public _trailheads: MapLocation[][] = [];
-
-  constructor(public map: number[][]) {
-    this._map = map;
-    for (let y = 0; y < map.length; y++) {
-      for (let x = 0; x < map[y].length; x++) {
-        const height = map[y][x];
-        if (height === 0) this._trailheads.push([{ x, y, height }]);
+  constructor(map: number[][], x: number, y: number) {
+    this._head = { x, y, height: map[y][x], parent: null, children: [] };
+    const queue = new Queue<Node>([this._head]);
+    while (!queue.isEmpty) {
+      const node = queue.dequeue();
+      if (node.height === 9) {
+        this._ends.add(`${node.x},${node.y}`);
+        this._rating++;
       }
-    }
-    this._findTrails();
-  }
-
-  private _isInBounds(x: number, y: number): boolean {
-    return x >= 0 && y >= 0 && x < this._map[0].length && y < this._map.length;
-  }
-
-  private _findTrails() {
-    for (const trail of this._trailheads) {
-      const queue = new Queue<MapLocation>([...trail]);
-      const visited = new Set<string>();
-      while (!queue.isEmpty) {
-        const { x, y, height } = queue.dequeue();
-        if (visited.has(`${x},${y}`)) continue;
-        visited.add(`${x},${y}`);
-        trail.push({ x, y, height });
-        for (const [dx, dy] of NEIGHBOURS) {
-          const xx = x + dx;
-          const yy = y + dy;
-          if (this._isInBounds(xx, yy) && this._map[yy][xx] === height + 1) {
-            const next = { x: xx, y: yy, height: height + 1 };
-            queue.enqueue(next);
-          }
+      for (const [dx, dy] of NEIGHBOURS) {
+        const xx = node.x + dx;
+        const yy = node.y + dy;
+        if (map[yy]?.[xx] === node.height + 1) {
+          const child = { x: xx, y: yy, height: map[yy][xx], parent: node, children: [] };
+          node.children.push(child);
+          queue.enqueue(child);
         }
       }
     }
   }
 
-  get trailheadSizes(): number[] {
-    return this._trailheads.map(t => t.filter(e => e.height === 9).length);
+  public traverse() {
+    const queue = new Queue<Node>([this._head]);
+    while (!queue.isEmpty) {
+      const node = queue.dequeue();
+      console.log(`(${node.x}, ${node.y}): ${node.height}`);
+      for (const child of node.children) {
+        queue.enqueue(child);
+      }
+    }
+  }
+
+  get size(): number {
+    return this._ends.size;
+  }
+
+  get rating(): number {
+    return this._rating;
+  }
+}
+
+class TopographicMap {
+  public _trailheads: Trail[] = [];
+
+  constructor(public map: number[][]) {
+    for (let y = 0; y < map.length; y++) {
+      for (let x = 0; x < map[y].length; x++) {
+        const height = map[y][x];
+        if (height === 0) {
+          const trail = new Trail(map, x, y);
+          this._trailheads.push(trail);
+        }
+      }
+    }
+  }
+
+  get sizes(): number[] {
+    return this._trailheads.map(t => t.size);
+  }
+
+  get ratings(): number[] {
+    return this._trailheads.map(t => t.rating);
   }
 }
 
 const map = new TopographicMap(input);
-console.log(sum(map.trailheadSizes));
+console.log(sum(map.sizes));
+console.log(sum(map.ratings));
