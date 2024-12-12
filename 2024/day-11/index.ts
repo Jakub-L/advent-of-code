34aeabe02573aa457ad37d139a7b354cabd01a7c
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2024-12-12 13:32:46 +0000

    Solve day 11

--- 2024/day-11/index.ts
+++ 2024/day-11/index.ts
@@ -1,20 +1,28 @@
+import { Counter } from "@jakub-l/aoc-lib/collections";
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 
 const input: number[] = readFile(`${__dirname}/input.txt`, [" "], Number) as number[];
 
 // Part 1
-const blink = (input: number[], times: number = 1): number[] => {
-  let result = input;
+const blink = (input: number[], times: number = 1): Counter => {
+  const nextValue = (n: number): number[] => {
+    const s = n.toString();
+    if (n === 0) return [1];
+    if (s.length % 2 === 0) return [Number(s.slice(0, s.length / 2)), Number(s.slice(s.length / 2))];
+    return [n * 2024];
+  };
+
+  let counter = new Counter(input);
   for (let i = 0; i < times; i++) {
-    result = result.flatMap(n => {
-      if (n === 0) return 1;
-      const s = n.toString();
-      if (s.length % 2 === 0) return [Number(s.slice(0, s.length / 2)), Number(s.slice(s.length / 2))];
-      return n * 2024;
-    });
+    const nextCounter = new Counter();
+    for (const [element, count] of counter.entries()) {
+      for (const val of nextValue(element)) {
+        nextCounter.set(val, nextCounter.get(val) + count);
+      }
+    }
+    counter = nextCounter;
   }
-  return result;
+  return counter;
 };
 
-// console.log(input);
-console.log(blink(input, 25).length);
+console.log(blink(input, 75).total);
