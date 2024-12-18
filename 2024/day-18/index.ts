17fc5b673c1938d1c352c50bda60853899fde6c7
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2024-12-18 14:20:36 +0000

    Solve day 18

--- 2024/day-18/index.ts
+++ 2024/day-18/index.ts
@@ -44,23 +44,22 @@ class MemorySpace {
   private _height: number;
   private _start: Point = { x: 0, y: 0 };
   private _end: Point;
-  private _corrupted: Set<string> = new Set();
+  private _allCorruptedBytes: string[];
 
   constructor(points: string[], width: number = 71, height: number = 71) {
     this._width = width;
     this._height = height;
     this._end = { x: width - 1, y: height - 1 };
-    for (const point of points) {
-      this._corrupted.add(point);
-    }
+    this._allCorruptedBytes = points;
   }
 
   private _isInBounds(x: number, y: number): boolean {
     return x >= 0 && x < this._width && y >= 0 && y < this._height;
   }
 
-  public traverse(): number {
+  public traverse(bytesFallen: number = 1024): number {
     const visited = new Set<string>();
+    const corrupted = new Set<string>(this._allCorruptedBytes.slice(0, bytesFallen));
     const queue: MinHeap<MemoryNode> = new MinHeap([{ ...this._start, dist: 0 }], e => e.dist);
 
     while (!queue.isEmpty) {
@@ -68,16 +67,25 @@ class MemorySpace {
       if (x === this._end.x && y === this._end.y) return dist;
       for (const [dx, dy] of DIR) {
         const [xx, yy] = [x + dx, y + dy];
-        if (!this._isInBounds(xx, yy) || visited.has(`${xx},${yy}`) || this._corrupted.has(`${xx},${yy}`)) continue;
+        if (!this._isInBounds(xx, yy) || visited.has(`${xx},${yy}`) || corrupted.has(`${xx},${yy}`)) continue;
         visited.add(`${xx},${yy}`);
         queue.add({ x: xx, y: yy, dist: dist + 1 });
       }
     }
     return -1;
   }
+
+  public firstBlockingByte(startingCount: number = 1024): string {
+    console.log(this._allCorruptedBytes.length);
+    for (let i = startingCount; i < this._allCorruptedBytes.length; i++) {
+      if (this.traverse(i) === -1) return this._allCorruptedBytes[i - 1];
+    }
+    return "";
+  }
 }
 
 // Results
-const memorySpace = new MemorySpace(input.slice(0, 1024));
+const memorySpace = new MemorySpace(input);
 
-console.log(memorySpace.traverse()); // 22
+// console.log(memorySpace.traverse()); // 334
+console.log(memorySpace.firstBlockingByte());
