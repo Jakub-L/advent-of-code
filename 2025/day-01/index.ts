9cc1226b2d352fba8e50d6ff2bab36e90675db86
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-12-01 11:53:32 +0000

    Solve day 1 part 2

--- 2025/day-01/index.ts
+++ 2025/day-01/index.ts
@@ -1,35 +1,44 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 
+// Constants
 const RING_SIZE = 100;
+const START_POSITION = 50;
+const DELTA = { R: 1, L: -1 };
 
+// Input
 const input: string[] = readFile(__dirname + "/input.txt");
 
-const testInput = `L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82`.split("\n");
-
-const step = (position: number, rotation: string): number => {
-  const [_, dir, steps] = /([RL])(\d+)/g.exec(rotation)!;
-  const sign = dir === "R" ? 1 : -1;
-  let newPosition = position + sign * Number(steps);
-  while (newPosition < 0) newPosition += RING_SIZE;
-  return newPosition % RING_SIZE;
-};
-
-const findPassword = (position: number, rotations: string[]): number => {
+// Part 1 & 2
+/**
+ * Applies a set of rotations to a safe ring and counts the number of times the pointer passes 0.
+ *
+ * @param startPosition - The starting position on the ring
+ * @param rotations - Array of rotations to perform
+ * @param countAllClicks - Whether any passing of 0 should count, or just
+ *                         the final position
+ * @returns The password, which is the number of times the pointer passes 0
+ */
+const findPassword = (
+  startPosition: number,
+  rotations: string[],
+  countAllClicks: boolean = false
+): number => {
   let password = 0;
+  let pos = startPosition;
+
   for (const rotation of rotations) {
-    position = step(position, rotation);
-    if (position === 0) password += 1;
+    const [_, newDir, steps] = /([RL])(\d+)/g.exec(rotation)!;
+    for (let i = 0; i < Number(steps); i++) {
+      pos += DELTA[newDir as "R" | "L"];
+      if (pos < 0) pos = 99;
+      if (pos > RING_SIZE - 1) pos = 0;
+      if (countAllClicks && pos === 0) password++;
+    }
+    if (!countAllClicks && pos === 0) password++;
   }
   return password;
 };
 
-console.log(findPassword(50, input));
+// Results
+console.log(`Part 1: ${findPassword(START_POSITION, input)}`);
+console.log(`Part 2: ${findPassword(START_POSITION, input, true)}`);
