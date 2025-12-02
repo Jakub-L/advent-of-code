149a0ead8ae29d4be39fdd2f8da898136c6a8f04
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-12-02 14:09:18 +0000

    Solve day 2 part 2

--- 2025/day-02/index.ts
+++ 2025/day-02/index.ts
@@ -2,29 +2,40 @@ import { readFile } from "@jakub-l/aoc-lib/input-parsing";
 
 // Input
 const input: number[][] = readFile(__dirname + "/input.txt", [",", "-"], Number) as number[][];
-const testInput: number[][] =
-  `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
-    .split(",")
-    .map(r => r.split("-").map(Number));
 
 // Part 1
-const findInvalidIds = (input: number[][]): number[] => {
+/**
+ * Finds the invalid IDs in the input array of product IDs.
+ *
+ * Does not differentiate between IDs that are the same number but generated different ways
+ * - e.g. 222222 is the same whether it is 22-22-22 or 222-222.
+ *
+ * @param productIds - The input array of produdct IDs, in the format of [start, end]
+ * @param onlyDoubles - Whether to only look for double IDs, e.g. 11, 123123, but not 123123123, etc.
+ * @returns An array of unique invalid IDs
+ */
+const findInvalidIds = (productIds: number[][], onlyDoubles: boolean = true): number[] => {
   const invalidIds: number[] = [];
-  const sortedInput = input.sort((a, b) => b[0] - a[0]);
-  const maxNumber = Math.max(...input.flat());
+  const sortedInput = productIds.sort((a, b) => b[0] - a[0]);
+  const maxNumber = Math.max(...productIds.flat());
   const maxDigits = maxNumber.toString().length;
 
-  for (let i = 0; i < 10 ** Math.ceil(maxDigits / 2); i++) {
-    const invalidId = Number(`${i}${i}`);
-    for (const [start, end] of sortedInput) {
-      if (invalidId > end) break;
-      if (invalidId >= start && invalidId <= end) {
-        invalidIds.push(invalidId);
+  for (let i = 1; i < 10 ** Math.ceil(maxDigits / 2); i++) {
+    let invalidId = Number(`${i}${i}`);
+    while (invalidId <= maxNumber) {
+      for (const [start, end] of sortedInput) {
+        if (invalidId > end) break;
+        if (invalidId >= start && invalidId <= end) {
+          invalidIds.push(invalidId);
+        }
       }
+      if (onlyDoubles) break;
+      invalidId = Number(`${invalidId}${i}`);
     }
   }
 
-  return invalidIds;
+  return Array.from(new Set(invalidIds));
 };
 
 console.log(`Part 1: ${findInvalidIds(input).reduce((a, b) => a + b, 0)}`);
+console.log(`Part 2: ${findInvalidIds(input, false).reduce((a, b) => a + b, 0)}`);
