11ffab1582ad959f4f681e4302ef072db247a5e4
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-12-03 12:43:28 +0000

    Solve day 3 part 2

--- 2025/day-03/index.ts
+++ 2025/day-03/index.ts
@@ -1,26 +1,40 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
+import { sum } from "@jakub-l/aoc-lib/math";
 
 // Input
 const input: number[][] = readFile(__dirname + "/input.txt", ["\n", ""], Number) as number[][];
-const testInput: number[][] = `987654321111111
-811111111111119
-234234234234278
-818181911112111`
-  .split("\n")
-  .map(line => line.split("").map(Number));
 
-// Part 1
-const findBankJoltage = (bank: number[]): number => {
-  let digits = [0, 0];
-  for (let i = 0; i < bank.length - 1; i++) {
-    if (bank[i] < digits[0]) continue;
-    for (let j = i + 1; j < bank.length; j++) {
-      const oldNumber = 10 * digits[0] + digits[1];
-      const newNumber = 10 * bank[i] + bank[j];
-      if (newNumber > oldNumber) digits = [bank[i], bank[j]];
+// Part 1 & 2
+/**
+ * Finds the maximum joltage of a bank of batteries.
+ * 
+ * This is the maximum number that can be formed by concatenating battery charge (numbers in the
+ * bank array). The batteries must be used in order they appear.
+ * 
+ * @param bank - The bank of batteries to find the joltage of
+ * @param batteryCount - The number of batteries to use to find the joltage
+ * @returns The joltage of the bank of batteries
+ */
+const findBankJoltage = (bank: number[], batteryCount: number = 2): number => {
+  let digits: number[] = [];
+  let startIndex = 0;
+
+  for (let n = 1; n <= batteryCount; n++) {
+    let currMax = 0;
+    let currIndex = 0;
+    for (let i = startIndex; i < bank.length - batteryCount + n; i++) {
+      if (bank[i] > currMax) {
+        currMax = bank[i];
+        currIndex = i + 1;
+      }
     }
+    digits.push(currMax);
+    startIndex = currIndex;
   }
-  return digits[0] * 10 + digits[1];
+
+  return digits.reduce((acc, d) => acc * 10 + d, 0);
 };
 
-console.log(input.map(findBankJoltage).reduce((acc, n) => acc + n, 0));
+// Results
+console.log(`Part 1: ${sum(input.map(n => findBankJoltage(n, 2)))}`);
+console.log(`Part 2: ${sum(input.map(n => findBankJoltage(n, 12)))}`);
