90de97f9221d4deb72f16759eee6d04e5a7ad0d6
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-12-12 21:16:25 +0000

    WIP: Create basic utility functions for solving day 10 via matrix method

--- 2025/day-10/index.ts
+++ 2025/day-10/index.ts
@@ -1,5 +1,5 @@
 import { readFile } from "@jakub-l/aoc-lib/input-parsing";
-import { sum } from "@jakub-l/aoc-lib/math";
+import { gcd, sum } from "@jakub-l/aoc-lib/math";
 
 // Types
 type Diagram = {
@@ -52,7 +52,69 @@ const findMinimumPresses = (diagram: Diagram): number => {
   return minimumPresses;
 };
 
+// Part 2
+class EquationSystem {
+  coefficients: number[][] = [];
+  counters: number[] = [];
+  bounds: number[] = [];
+
+  constructor(diagram: Diagram) {
+    this.counters = diagram.joltage;
+    this.coefficients = Array.from({ length: this.counters.length }, _ => []);
+    for (const button of diagram.buttons) {
+      const buttonSet = new Set(button);
+      this.bounds.push(Math.min(...button.map(n => this.counters[n])));
+      for (let i = 0; i < this.counters.length; i++) {
+        this.coefficients[i].push(buttonSet.has(i) ? 1 : 0);
+      }
+    }
+  }
+
+  public _swapRows(i: number, j: number) {
+    if (i >= this.coefficients.length || j >= this.coefficients.length) return;
+    if (i === j) return;
+
+    [this.coefficients[i], this.coefficients[j]] = [
+      this.coefficients[j],
+      this.coefficients[i]
+    ];
+    [this.counters[i], this.counters[j]] = [this.counters[j], this.counters[i]];
+  }
+
+  public _swapCols(i: number, j: number) {
+    if (i >= this.coefficients[0].length || j >= this.coefficients[0].length) return;
+    if (i === j) return;
+
+    for (let k = 0; k < this.coefficients.length; k++) {
+      [this.coefficients[k][i], this.coefficients[k][j]] = [
+        this.coefficients[k][j],
+        this.coefficients[k][i]
+      ];
+    }
+    [this.bounds[i], this.bounds[j]] = [this.bounds[j], this.bounds[i]];
+  }
+
+  public _reduceRow(pivotRow: number, targetRow: number) {
+    const x = this.coefficients[pivotRow][pivotRow];
+    if (x === 0) return;
+    const y = -this.coefficients[targetRow][pivotRow];
+    const divisor = gcd(x, y);
+    this.coefficients[targetRow] = this.coefficients[pivotRow].map(
+      (_, col) =>
+        (y * this.coefficients[pivotRow][col] + x * this.coefficients[targetRow][col]) /
+        divisor
+    );
+    this.counters[targetRow] =
+      (y * this.bounds[pivotRow] + x * this.bounds[targetRow]) / divisor;
+  }
+}
+
 // console.log(findMinimumPresses(testInput[0]));
 // console.log(findMinimumPresses(testInput[1]));
 // console.log(findMinimumPresses(testInput[2]));
-console.log(sum(input.map(findMinimumPresses)));
+// console.log(sum(input.map(findMinimumPresses)));
+const eq = new EquationSystem(testInput[0]);
+
+console.log(eq.coefficients);
+eq._swapCols(0, 3);
+console.log(eq.coefficients);
